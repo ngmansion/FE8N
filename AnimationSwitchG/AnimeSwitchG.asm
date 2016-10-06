e059fa4de6b3564aa1fb9e787661f334d6e68f6d
ngmansion <eliwooood@gmail.com>
2016-10-07 08:47:09 +0900

    fix

--- AnimationSwitchG/AnimeSwitchG.asm
+++ AnimationSwitchG/AnimeSwitchG.asm
@@ -13,7 +13,7 @@ tugi
 san
 	mov	r0, #3
 end
-	ldr	r1, [adr4]
+	ldr	r1, [adr4]	;$02024CC0
 	ldrh	r1, [r1, #4]
 	lsl	r1, r1, #22
 	bpl	nonpress
@@ -32,13 +32,13 @@ nonpress
 	ldrb	r0, [r0, #0xF]
 	lsr	r0, r0, #6
 	beq	non
-	ldr	r1, [adr4]
+	ldr	r1, [adr4]	;$02024CC0
 	ldrh	r1, [r1, #4]
 	lsl	r1, r1, #31
 	bpl	non
 	b	zero
 non
-	ldr	r0, [adr1+4]
+	ldr	r0, [adr1+4]	;$0202bcec
 	add	r0, #66
 	ldrb	r0, [r0]
 	lsl	r0, r0, #29
@@ -46,20 +46,20 @@ non
 	cmp	r0, #2
 	bne	end		;個別以外は終了。オフ=1, 背景=3
 ;;個別の処理
-	ldr	r2, [adr2]
+	ldr	r2, [adr2]	;$0203a4e8
 	mov	r0, #11
 	ldsb	r0, [r2, r0]
 	lsl	r0, r0, #24
 	bmi	next
-	ldr	r0, [adr3+4]
+	ldr	r0, [adr3+4]	;$0203a568
 	mov	r1, #0x0B
 	ldsb	r0, [r0, r1]
 	lsl	r0, r0, #24
 	bmi	two
-	ldr	r0, [adr2]
+	ldr	r0, [adr2]	;$0203a4e8
 	b	three
 next
-	ldr	r2, [adr3]
+	ldr	r2, [adr3]	;$0203a568
 	mov	r0, #11
 	ldsb	r0, [r2, r0]
 	lsl	r0, r0, #24
