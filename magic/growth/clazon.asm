6a79043ecf818a2a0b3610a01b32eea331962a5f
ngmansion <eliwooood@gmail.com>
2016-12-13 12:21:19 +0900

    add

--- magic/growth/clazon.asm
+++ magic/growth/clazon.asm
@@ -1,5 +1,12 @@
 @thumb
+
+		ldr	r2, =$0802b90c
+		mov	lr, r2
+		@dcw	$F800
+	ldrb	r1, [r4, #25]
+	add	r1, r1, r0
 	strb	r1, [r4, #25]
+	
 	ldr	r0, [r4, #4]
 	add	r0, #81
 	ldrb	r0, [r0, #0]	;クラス成長率読み
