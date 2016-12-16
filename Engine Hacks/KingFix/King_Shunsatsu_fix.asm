90b59c873d26fcefff48e53e78606bf34255cd43
ngmansion <eliwooood@gmail.com>
2016-12-17 00:45:30 +0900

    change

---
+++ Engine Hacks/KingFix/King_Shunsatsu_fix.asm
@@ -0,0 +1,26 @@
+@thumb
+@org	$0802b2fc
+	push	{lr}
+	ldr	r0, [r1, #4]
+	ldrb	r0, [r0, #4]
+	cmp	r0, #0x66	;;クラス魔王は瞬殺不可
+	beq	non
+	ldr	r0, [adr1+4]
+	ldrh	r0, [r0, #14]
+	cmp	r0, #0
+	beq	non
+	mov	r1, #0
+	bl	$0802a490 ;;r0=確率, r1=#0 乱数
+	lsl	r0, r0, #24
+	asr	r0, r0, #24
+	cmp	r0, #1
+	beq	go
+non
+	mov	r0, #0
+	b	end
+go
+	mov	r0, #1
+end
+	pop	{pc}
+adr1
+@dcd	$0203a4d0
