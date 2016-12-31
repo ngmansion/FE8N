850d7ca422a33fb8bdc24abc850cc273c8ebe6b9
ngmansion <eliwooood@gmail.com>
2017-01-01 04:12:33 +0900

    fix

--- Engine Hacks/EquipmentPopup/power_view.asm
+++ Engine Hacks/EquipmentPopup/power_view.asm
@@ -1,7 +1,9 @@
 @thumb
-;@org	0008f2b4
+;@org	0008e75c
 
-	push	{r4, r5, r6, r7}
+	push	{r4, r5, r6, r7, lr}
+	mov	r7, r8
+	push	{r7}
 	mov	r4, r0
 	mov	r5, r1
 
@@ -34,14 +36,31 @@
 	mov	r1, r4
 	add	r1, #84
 	strb	r0, [r1, #0]
-	pop	{r4, r5, r6, r7}
-;元処理
-	pop	{r4, r5, r6}
-	pop	{r0}
-	bx	r0
+;文字
+	ldr	r1, [r4, #64]
+	mov	r0, r4
+	mov	r2, #0
+	ldr	r3, =$2160
+	strh	r3, [r1, #0]
+	add	r3, #1
+	strh	r3, [r1, #2]
+	strh	r2, [r1, #4]
+	strh	r2, [r1, #6]
+	add	r3, #1
+	strh	r3, [r1, #8]
+	strh	r2, [r1, #10]
+	strh	r2, [r1, #12]
+	mov	r0, #1
+	ldr	r1, =$08001efc
+	mov	lr, r1
+	@dcw	$F800
 	
+	ldr	r0, =$0808e838
+	mov	pc, r0
+
 numbers:
 	ldr	r1, =$08003868
 	mov	pc, r1
 	
+
 	
