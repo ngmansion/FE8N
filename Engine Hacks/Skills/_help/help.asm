6b140f45d9a1ab0381b94150ec0b006856d9bc21
ngmansion <eliwooood@gmail.com>
2017-01-21 18:39:45 +0900

    ちょっと機能追加

--- Engine Hacks/Skills/_help/help.asm
+++ Engine Hacks/Skills/_help/help.asm
@@ -1,48 +1,22 @@
 @thumb
-	ldr	r1, [adr+4]
-	add	r0, #76
-	ldrh	r1, [r1]
-	cmp	r1, #0
-	beq	end
-	strh	r1, [r0]
-	bx	lr
-	
-	ldr	r1, [adr+4]
-	add	r1, #2
-	add	r0, #76
-	ldrh	r1, [r1]
-	cmp	r1, #0
-	beq	end
-	strh	r1, [r0]
-	bx	lr
-	
-	ldr	r1, [adr+4]
-	add	r1, #4
-	add	r0, #76
-	ldrh	r1, [r1]
-	cmp	r1, #0
-	beq	end
-	strh	r1, [r0]
-	bx	lr
-	
-	ldr	r1, [adr+4]
-	add	r1, #6
-	add	r0, #76
-	ldrh	r1, [r1]
-	cmp	r1, #0
-	beq	end
-	strh	r1, [r0]
-	bx	lr
-	
-	ldr	r1, [adr+4]
-	add	r1, #8
-	add	r0, #76
-	ldrh	r1, [r1]
-	cmp	r1, #0
-	beq	end
-	strh	r1, [r0]
-end
-	bx	lr
 
-adr
-@dcd	$02003B00
+	mov	r1, #0
+	b merge
+	mov	r1, #2
+	b merge
+	mov	r1, #4
+	b merge
+	mov	r1, #6
+	b merge
+	mov	r1, #8
+	b merge
+	mov	r1, #10
+merge:
+	add	r0, #76
+	ldr r2 =$02003B00
+	ldrh	r1, [r2, r1]
+	cmp	r1, #0
+	beq	end
+	strh	r1, [r0]
+end:
+	bx	lr
--- Engine Hacks/Skills/iconEX.asm
+++ Engine Hacks/Skills/iconEX.asm
@@ -4,7 +4,7 @@
 	push	{r4, r5, r6, r7, lr}
 ;画像
 	mov	r4, #0
-	ldr	r0, =$02003DAC
+	ldr	r0, =$02003DAC ;画像($02003DEC)
 	ldr	r6, =$00007060
 	mov	r5, r6	
 	ldr	r1, =$000002c2
@@ -24,12 +24,19 @@ loopE
 	cmp	r4, #7
 	ble	loopE
 ;アイコン
-	ldr	r6, =$020040F0	;icon position
+	ldr	r6, =$020040F0	;icon position($02004130)
 	ldr	r7, =$02003B00	;help memory
 	mov	r5, #0
 	str	r5, [r7]
 	str	r5, [r7, #4]
 	str	r5, [r7, #8]
+@align 4
+	ldr	r5, [adr+12]
+	ldr	r4, =$02003BFC
+	ldr	r4, [r4, #12]
+	add	r4, #0x3B
+	ldrb	r4, [r4]
+	bl	SKILL2
 @align 4
 	ldr	r5, [adr]
 	ldr	r4, =$02003BFC
@@ -57,9 +64,7 @@ end
 
 SKILL
 	push	{lr}
-	cmp	r5, #0
-	bne	test
-	pop	{pc}
+	b	test
 restart
 	mov	r0, r4
 	mov	r2, #0x10
@@ -120,6 +125,74 @@ limitter
 icon
 	ldr	r3, =$08003608
 	mov	pc, r3
+	
+
+
+SKILL2
+	push	{lr}
+	cmp	r4, #0
+	bne	test2
+	pop	{pc}
+
+restart2
+	mov	r0, r4
+	mov	r2, #0x10
+loop2
+	ldrb	r1, [r5, r2]
+	cmp	r1,	#0
+	beq	jump2
+	and r1, r0
+	cmp	r0, r1
+	beq	skiller2
+	add	r2, #1
+	cmp	r2, #0x20
+	beq	jump2 ;LISTlimit
+	b	loop2
+
+skiller2
+	ldrh	r1, [r5, #2]
+	strh	r1, [r7]
+	ldrh	r1, [r5]
+	mov	r2, #0xA0
+	ldrb	r3, [r5, #4]
+	cmp	r3, #0xFF
+	bne	nonitem2
+	sub	r2, #0x20
+nonitem2
+	lsl	r2, r2, #7
+	mov	r0, r6
+	bl	icon
+	add	r6, #6	;アイコンの間隔
+	add	r7, #2	;HELP memory increment
+	
+jump2
+	add	r5, #0x20
+test2
+	ldrb	r0, [r5]
+	cmp	r0, #0xFF
+	bne	doublecount2
+	pop	{pc}
+
+doublecount2
+	ldrh r0, [r5, #2]
+	ldr r2 =$02003B00
+loopyloopy2
+	cmp r2, r7
+	beq limitter2
+	ldrh r1, [r2]
+	cmp r0, r1
+	beq jump2
+	add r2, #2
+	b loopyloopy2
+limitter2
+	lsl r0, r7, #24
+	lsr r0, r0, #24
+	cmp r0, #10
+	bne restart2
+	pop {pc}
+	
+	
+	
 
 @ltorg
 adr:
