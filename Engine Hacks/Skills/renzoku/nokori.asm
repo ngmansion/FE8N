17e462112c539243458776318315d0349e301d80
aera <eliwooood@gmail.com>
2017-02-22 13:23:43 +0900

    処理を飛ばしすぎていた問題修正

--- Engine Hacks/Skills/renzoku/nokori.asm
+++ Engine Hacks/Skills/renzoku/nokori.asm
@@ -5,17 +5,6 @@
 	beq	teki
 	ldr	r0, =$0802b372
 	mov	pc, r0
-teki
-	mov	r6, r5
-	ldr	r5, =$0203a4d0
-	mov	r1, #6
-	ldsh	r1, [r5, r1]
-	mov	r9, r1
-	
-	mov	r0, #8
-	ldsh	r4, [r5, r0]
-	
-	sub	r0, r1, r4
-	strh	r0, [r5, #4]
-	ldr	r0, =$0802b402
+teki:
+	ldr	r0, =$0802b3a4
 	mov	pc, r0
