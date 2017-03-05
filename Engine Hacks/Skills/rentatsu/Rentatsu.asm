3c01f38e609fa89c8bcf307e487fa196b66a0ecc
ngmansion <eliwooood@gmail.com>
2017-03-05 21:58:03 +0900

    正常に動作しないバグ修正

--- Engine Hacks/Skills/rentatsu/Rentatsu.asm
+++ Engine Hacks/Skills/rentatsu/Rentatsu.asm
@@ -1,9 +1,11 @@
 @thumb
 ;@org	$080169a4 > 00 48 87 46 XX XX XX 08
 
+@align 4
 	ldr	r1, =$0203a568
 	cmp	r1, r4
 	bne	nonchange
+@align 4
 	ldr	r1, =$0203a4e8
 nonchange
 ;見切りチェック
@@ -46,6 +48,7 @@ NIHIL
 	and	r0, r3
 	lsl	r1, r0, #3
 	add	r1, r1, r0
+@align 4
 	ldr	r5, =$080169ac
 	mov	pc, r5
 zero
@@ -53,6 +56,7 @@ zero
 	pop	{r4, r5, r6}
 	pop	{r1}
 	bx	r1
+@ltorg
 adr:
 ;memo
 ;R04=0203a4e8
