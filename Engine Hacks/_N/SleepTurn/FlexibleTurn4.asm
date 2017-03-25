b64a495f6252350475bd2008b6bd70caf1e824f9
ngmansion <eliwooood@gmail.com>
2017-03-25 16:19:09 +0900

    change

--- Engine Hacks/_N/SleepTurn/FlexibleTurn4.asm
+++ Engine Hacks/_N/SleepTurn/FlexibleTurn4.asm
@@ -1,10 +1,14 @@
 @thumb
 ;@org	$08017680 > 00 4A 97 46 XX XX XX XX
-	
-	ldr	r2, [r0]
+;クラス性能に経験値ゼロがついていた場合、状態異常無効
 	ldr	r3, [r0, #4]
-	ldr	r2, [r2, #40]
 	ldr	r3, [r3, #40]
+	lsl	r2, r3, #7
+	bpl	Normal
+	bx	lr
+Normal:
+	ldr	r2, [r0]
+	ldr	r2, [r2, #40]
 	orr	r2, r3
 	ldrb	r3, [r0, #11]	;所属ID
 	add	r0, #48
@@ -19,7 +23,7 @@
 noone
 	mov	r2, #0xF0
 	and	r2, r1
-	bne	end
+	bne	end	;ターン指定があるならジャンプ
 	add	r1, #0x30	;3ターン
 	lsr	r3, r3, #6
 	lsl	r3, r3, #30
