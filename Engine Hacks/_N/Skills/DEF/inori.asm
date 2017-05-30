dd51563eac406c4998c69cbc8c040f7df08d461e
ngmansion <eliwooood@gmail.com>
2017-05-30 22:31:38 +0900

    聖盾追加

--- Engine Hacks/_N/Skills/DEF/inori.asm
+++ Engine Hacks/_N/Skills/DEF/inori.asm
@@ -12,7 +12,44 @@
 	beq	end
 	mov	r1, #0xDF		;防御用
 	mov	r10, r1
-;;;;;;;;;;;;;大盾
+	
+bl BigShield
+	cmp	r0, #1
+	beq	effect
+bl HolyShield
+	cmp	r0, #1
+	beq	effect
+bl Pray
+	cmp	r0, #1
+	beq	effect
+bl Oracle
+	b	end
+
+effect:
+	ldr	r3, =$0203a604
+	ldr	r3, [r3]
+	ldr	r2, [r3]
+	lsl	r1, r2, #13
+	lsr	r1, r1, #13
+	mov	r0, #128
+	lsl	r0, r0, #8
+	orr	r1, r0
+	ldr	r0, =$FFF80000
+	and	r0, r2
+	orr	r0, r1
+	str	r0, [r3]
+	b	end
+	
+zero:
+	mov	r0, #0
+	strh	r0, [r4, #4]
+end:
+	ldr	r0, =$0802b49a
+	mov	pc, r0
+	
+
+BigShield:
+	push {lr}
 @align 4
 	ldr	r2, [adr]	;大盾クラスアドレス
 	ldr	r1, [r3, #4]
@@ -32,82 +69,130 @@ skill1_check
 	ldrh	r0, [r3, #0x3A]
 	orr r1, r0
 	lsl	r1, r1, #31
-	bpl	skill2
+	bpl	endShield
 ootate
 	mov	r0, #0x50
 	ldrb	r0, [r7, r0]	;魔法判定
 	cmp	r0, #7
-	beq	skill2
+	beq	endShield
 	cmp	r0, #6
-	beq	skill2
+	beq	endShield
 	cmp	r0, #5
-	beq	skill2
+	beq	endShield
 
 	
 	ldrb	r0, [r3, #21]	;技
 	mov	r1, #0
 	bl	random
 	cmp	r0, #0
-	beq	skill2
+	beq	endShield
 	ldrh	r0, [r4, #4]
 	lsr	r0, r0, #1
 	strh	r0, [r4, #4]
-	b	effect
-;;;;;;祈り
-skill2
+	mov	r0, #1
+	pop	{pc}
+
+endShield:
+	mov	r0, #0
+	pop	{pc}
+	
+	
+HolyShield:
+	push {lr}
+@align 4
+	ldr	r2, [adr+4]	;聖盾クラスアドレス
+	ldr	r1, [r3, #4]
+	ldrb	r1, [r1, #4]
+loopHoly
+	ldrb	r0, [r2]
+	cmp	r0, #0
+	beq	unitHoly
+	cmp	r0, r1
+	beq	Holy
+	add	r2, #1
+	b	loopHoly
+;ユニットチェック
+unitHoly
+	ldr	r1, [r3]
+	add	r1, #0x31
+	ldrb	r0, [r1]
+	cmp r0, #6
+	bne	endHoly
+Holy
+	mov	r0, #0x50
+	ldrb	r0, [r7, r0]	;物理判定
+	cmp	r0, #7
+	beq	onHoly
+	cmp	r0, #6
+	beq	onHoly
+	cmp	r0, #5
+	beq	onHoly
+	b endHoly
+onHoly:
+	ldrb	r0, [r3, #21]	;技
+	mov	r1, #0
+	bl	random
+	cmp	r0, #0
+	beq	endHoly
+	ldrh	r0, [r4, #4]
+	lsr	r0, r0, #1
+	strh	r0, [r4, #4]
+	mov	r0, #1
+	pop	{pc}
+
+endHoly:
+	mov	r0, #0
+	pop	{pc}
+	
+	
+Pray:
+	push {lr}
 	mov	r3, r8
 	ldrb	r1, [r3, #19]
 	cmp	r1, #1
-	beq	skill3
+	beq	endPray
 ;一撃で死ぬか
 	ldrh	r0, [r4, #4]
 	cmp	r0, r1
-	blt	skill3
+	blt	endPray
 ;ダメージが幸運を上回るか
 ;	ldrb	r1, [r3, #25]
 ;	cmp	r0, r1
-;	bgt	skill3
+;	bgt	endPray
 	
 	ldr	r0, [r3]
 	ldrh	r0, [r0, #0x26]
 	ldrh	r1, [r3, #0x3A]
 	orr r0, r1
 	lsl	r0, r0, #30
-	bpl	skill3
+	bpl	endPray
 	ldrb	r0, [r3, #25]	;幸運
 	lsl	r1, r0, #1
 	add	r0, r0, r1
 	mov	r1, #0
 	bl	random
 	cmp	r0, #0
-	beq	skill3
+	beq	endPray
 	mov	r0, r8
 	ldrb	r0, [r0, #19]
 	sub	r0, #1
 	strh	r0, [r4, #4]
-effect
-	ldr	r3, =$0203a604
-	ldr	r3, [r3]
-	ldr	r2, [r3]
-	lsl	r1, r2, #13
-	lsr	r1, r1, #13
-	mov	r0, #128
-	lsl	r0, r0, #8
-	orr	r1, r0
-	ldr	r0, =$FFF80000
-	and	r0, r2
-	orr	r0, r1
-	str	r0, [r3]
-	b	end
-;;;;;;;;;;オラクル
-skill3
+	mov	r0, #1
+	pop	{pc}
+endPray:
+	mov	r0, #0
+	pop	{pc}
+
+
+Oracle:
+	push	{lr}
 	ldr	r1, [r3, #4]
 	ldrb	r1, [r1, #4]
 	cmp	r1, #0x00		;無指定
 	beq	nihil_check
 	cmp	r1, #0x66		;魔王
 	beq	nihil_check
-	b	end
+	b	endOracle
 nihil_check
 	ldr	r1, [r7]
 	ldr	r0, [r7, #4]
@@ -125,7 +210,7 @@ nihil_check
 	ldrh	r0, [r4, #4]
 	asr	r0, r0, #1
 	strh	r0, [r4, #4]
-	b	end
+	b	endOracle
 Nihil
 	ldrh	r0, [r4, #4]
 	lsl	r0, r0, #1
@@ -136,15 +221,11 @@ division
 	bgt	division
 	sub	r1, #1
 	strh	r1, [r4, #4]
-	b	end
-zero
-	mov	r0, #0
-	strh	r0, [r4, #4]
-end	
-	ldr	r0, =$0802b49a
-	mov	pc, r0
+endOracle:
+	pop	{pc}
+
 	
-random
+random:
 	ldr	r2, =$0802a490
 	mov	pc, r2
 @ltorg
