6dfb5cb2eb534f27e257bd1071897268e520e78a
ngmansion <eliwooood@gmail.com>
2017-07-15 15:26:32 +0900

    地形回復のバグ修正

--- Engine Hacks/_N/_Skills/Kaifuku/KAIFUKU.asm
+++ Engine Hacks/_N/_Skills/Kaifuku/KAIFUKU.asm
@@ -1,14 +1,15 @@
 @thumb
 ;@org	$08019f28
+	mov	r2, r0
 	ldr	r1, [r5]
-	ldrh	r1, [r1, #0x26]	;;ƒ†ƒjƒbƒg0x1000
+	ldrh	r1, [r1, #0x26]	;;ãƒ¦ãƒ‹ãƒƒãƒˆ0x1000
 	lsl	r1, r1, #19
 	bpl	classHeal
 	mov	r3, #20
 	b	end
 classHeal:
 @align 4
-	ldr	r3, [adr]	;‰ñ•œƒNƒ‰ƒXƒAƒhƒŒƒX
+	ldr	r3, [adr]	;å›žå¾©å…µç¨®ã‚¢ãƒ‰ãƒ¬ã‚¹
 	ldr	r1, [r5, #4]
 	ldrb	r1, [r1, #4]
 loopHeal
@@ -27,7 +28,7 @@ nonHeal:
 end:
 	ldr	r1, =$08019f34
 	ldr	r1, [r1]
-	add	r0, r0, r1
+	add	r0, r2, r1
 	ldrb	r0, [r0]
 	lsl	r0, r0, #24
 	asr	r0, r0, #24
