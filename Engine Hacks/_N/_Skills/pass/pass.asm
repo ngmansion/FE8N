580ca17408b7a716264e375209fecd56722c63d0
ngmansion <eliwooood@gmail.com>
2017-07-20 22:54:42 +0900

    すり抜け更新

--- Engine Hacks/_N/_Skills/pass/pass.asm
+++ Engine Hacks/_N/_Skills/pass/pass.asm
@@ -1,28 +1,48 @@
 @thumb
+	
+
 	cmp	r3, #0
 	bne	Start
 	strb	r3, [r6, #8]
 	ldr	r0, =$0801a1ea
 	mov	pc, r0
 Start:
+;
+;索敵マップなら無効
+;
+	ldr	r0, =$0202BCF9
+	ldrb	r0, [r0]
+	cmp	r0, #0
+	bne	nonPass
+;
+;謎のバグ防止
+;
 	mov	r0, r13
 	ldr	r1, =$03007d18
 	cmp	r0, r1
 	beq	nonPass
+;
+;個人スキルチェック
+;
 	ldr	r0, [r4, #0]
 	ldrh	r0, [r0, #38]
 	lsl	r0, r0, #20
 	bmi	ouiPass
-	ldr	r0, [r4, #4]
-	ldrb	r0, [r0, #4]
-	cmp	r0, #23
-	beq	ouiPass
-	cmp	r0, #24
-	beq	ouiPass
+;
+;兵種スキルチェック
+;
+@align 4
+	ldr	r2, [adr]	;すり抜け兵種ドレス
+	ldr	r1, [r4, #4]
+	ldrb	r1, [r1, #4]
+loopPass:
+	ldrb	r0, [r2]
 	cmp	r0, #0
+	beq	nonPass
+	cmp	r0, r1
 	beq	ouiPass
-	cmp	r0, #0
-	beq	ouiPass
+	add	r2, #1
+	b	loopPass
 nonPass:
 	mov	r0, #1
 	b	Return
@@ -31,3 +51,5 @@ ouiPass:
 Return:
 	ldr	r1, =$0801a1e6
 	mov	pc, r1
+@ltorg
+adr:
