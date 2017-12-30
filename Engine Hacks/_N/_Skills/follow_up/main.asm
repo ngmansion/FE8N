a19074124eed3ba2d33cdaaa04f4579ff0244699
ngmansion <eliwooood@gmail.com>
2017-12-30 22:59:53 +0900

    追撃関連スキル
    攻撃隊形・迎撃隊形に対応
    武器スキル化に対応

--- Engine Hacks/_N/_Skills/follow_up/main.asm
+++ Engine Hacks/_N/_Skills/follow_up/main.asm
@@ -3,9 +3,8 @@
     mov r5, r0
     
     bl waryFighter_judgeActivate
-    
     push {r0}
-    bl breaker_judgeActivate
+    bl followup_skill
     pop {r1}
     
     cmp r0, #0x00
@@ -41,142 +40,217 @@ normal:
     ldr r1, =$0802af24
     mov pc, r1
 
-waryFighter_judgeActivate:
 
-push {r7, lr}
-    mov r0, r6
-    mov r1, r5
-    bl waryFighter_impl
-    mov r7, r0
-    
-    mov r0, r5
-    mov r1, r6
-    bl waryFighter_impl
-    orr r0, r7
-pop {r7, pc}
-    
-waryFighter_impl:
-push {r4, r5, lr}
-    mov r4, r0
-    mov r5, r1
-    
-    mov r0, r5
-        @align 4
-        ldr r1, [Adr+20] ;見切り
-        mov lr, r1
-        @dcw $F800
-    cmp r0, #0
-    bne	non_waryFighter_impl
-    
-    mov r0, r4
-        @align 4
-        ldr r1, [Adr+24] ;守備隊形
-        mov lr, r1
-        @dcw $F800
-    cmp r0, #0
-    beq	non_waryFighter_impl
-    
-    mov r0, #1
-pop {r4, r5, pc}
-    
-non_waryFighter_impl:
-    mov r0, #0
-pop {r4, r5, pc}
-
-
-breaker_judgeActivate:
-push {r7,lr}
-    mov r7, #0
-
-    mov r0, r6
-    mov r1, r5
-    bl breaker_impl
-    orr r7, r0
-    
-    mov r0, r5
-    mov r1, r6
-    bl breaker_impl
-    lsl r0, r0, #4
-    orr r7, r0
-    mov r0, r7
-pop {r7,pc}
-
-
-breaker_impl:
-push {r4, r5, lr}
-    mov r4, r0
-    mov r5, r1
-    
-    mov r0, r5
-        @align 4
-        ldr r1, [Adr+20] ;見切り
-        mov lr, r1
-        @dcw $F800
-    cmp r0, #0
-    bne	end
-        ldr r0, =$080172f0
-        mov lr, r0
+    waryFighter_judgeActivate:
+    push {r7, lr}
+        mov r0, r6
+        mov r1, r5
+        bl waryFighter_impl
+        mov r7, r0
+        
         mov r0, r5
-        add	r0, #74
-        ldrh r0, [r0]
-        @dcw $F800
-    cmp r0, #0
-    beq sword
-    cmp r0, #1
-    beq lance
-    cmp r0, #2
-    beq axe
-    cmp r0, #3
-    beq bow
-    cmp r0, #4
-    beq end
-    cmp r0, #7
-    ble magic
-    b end
-    
-sword:
-    @align 4
-    ldr r0, [Adr]
-    mov lr, r0
-    mov r0, r4
-    @dcw $F800
-    b merge
-lance:
-    @align 4
-    ldr r0, [Adr+4]
-    mov lr, r0
-    mov r0, r4
-    @dcw $F800
-    b merge
-axe:
-    @align 4
-    ldr r0, [Adr+8]
-    mov lr, r0
-    mov r0, r4
-    @dcw $F800
-    b merge
-bow:
-    @align 4
-    ldr r0, [Adr+12]
-    mov lr, r0
-    mov r0, r4
-    @dcw $F800
-    b merge
-magic:
-    @align 4
-    ldr r0, [Adr+16]
-    mov lr, r0
-    mov r0, r4
-    @dcw $F800
-merge:
-    cmp r0, #0
-    beq end
-    
-    mov r0, #1
-pop {r4, r5, pc}
-end:
-    mov r0, #0
-pop {r4, r5, pc}
+        mov r1, r6
+        bl waryFighter_impl
+        orr r0, r7
+    pop {r7, pc}
+        
+        waryFighter_impl:
+        push {r4, r5, lr}
+            mov r4, r0
+            mov r5, r1
+            
+            mov r0, r5
+                @align 4
+                ldr r1, [Adr+20] ;見切り
+                mov lr, r1
+                @dcw $F800
+            cmp r0, #0
+            bne	non_waryFighter_impl
+            
+            mov r0, r4
+                @align 4
+                ldr r1, [Adr+24] ;守備隊形
+                mov lr, r1
+                @dcw $F800
+            cmp r0, #0
+            beq	non_waryFighter_impl
+            
+            mov r0, #1
+            @dcw $E000
+        non_waryFighter_impl:
+            mov r0, #0
+        pop {r4, r5, pc}
+
+
+    followup_skill:
+    push {r7, lr}
+        bl formation_judgeActivate
+        mov r7, r0
+        bl breaker_judgeActivate
+        orr r0, r7
+    pop {r7, pc}
+
+        formation_judgeActivate:
+        push {r7, lr}
+            mov r0, r5
+            mov r1, r6
+            bl boldFighter ;攻撃隊形
+            mov r7, r0
+            
+            mov r0, r6
+            mov r1, r5
+            bl vengefulFighter ;迎撃隊形
+            lsl r0, r0, #4
+            orr r0, r7
+        pop {r7, pc}
+
+
+            boldFighter:
+            push {r4, r5, lr}
+                mov r4, r0
+                mov r5, r1
+                
+                mov r0, r5
+                    @align 4
+                    ldr r1, [Adr+20] ;見切り
+                    mov lr, r1
+                    @dcw $F800
+                cmp r0, #0
+                bne	non_bold
+                
+                mov r0, r4
+                    @align 4
+                    ldr r1, [Adr+28] ;攻撃隊形
+                    mov lr, r1
+                    @dcw $F800
+                cmp r0, #0
+                beq	non_bold
+                mov r0, #1
+                @dcw $E000
+            non_bold:
+                mov r0, #0
+            pop {r4, r5, pc}
+
+            vengefulFighter:
+            push {r4, r5, lr}
+                mov r4, r0
+                mov r5, r1
+                
+                mov r0, r5
+                    @align 4
+                    ldr r1, [Adr+20] ;見切り
+                    mov lr, r1
+                    @dcw $F800
+                cmp r0, #0
+                bne	non_vengeful
+                
+                mov r0, r4
+                    @align 4
+                    ldr r1, [Adr+32] ;迎撃隊形
+                    mov lr, r1
+                    @dcw $F800
+                cmp r0, #0
+                beq	non_vengeful
+                mov r0, #1
+                @dcw $E000
+            non_vengeful:
+                mov r0, #0
+            pop {r4, r5, pc}
+
+
+
+        breaker_judgeActivate:
+        push {r7,lr}
+            mov r7, #0
+
+            mov r0, r5
+            mov r1, r6
+            bl breaker_impl
+            orr r7, r0
+            
+            mov r0, r6
+            mov r1, r5
+            bl breaker_impl
+            lsl r0, r0, #4
+            orr r0, r7
+        pop {r7,pc}
+
+
+            breaker_impl:
+            push {r4, r5, lr}
+                mov r4, r0
+                mov r5, r1
+                
+                mov r0, r5
+                    @align 4
+                    ldr r1, [Adr+20] ;見切り
+                    mov lr, r1
+                    @dcw $F800
+                cmp r0, #0
+                bne	end
+                    ldr r0, =$080172f0
+                    mov lr, r0
+                    mov r0, r5
+                    add	r0, #74
+                    ldrh r0, [r0]
+                    @dcw $F800
+                cmp r0, #0
+                beq sword
+                cmp r0, #1
+                beq lance
+                cmp r0, #2
+                beq axe
+                cmp r0, #3
+                beq bow
+                cmp r0, #4
+                beq end
+                cmp r0, #7
+                ble magic
+                b end
+                
+            sword:
+                @align 4
+                ldr r0, [Adr]
+                mov lr, r0
+                mov r0, r4
+                @dcw $F800
+                b merge
+            lance:
+                @align 4
+                ldr r0, [Adr+4]
+                mov lr, r0
+                mov r0, r4
+                @dcw $F800
+                b merge
+            axe:
+                @align 4
+                ldr r0, [Adr+8]
+                mov lr, r0
+                mov r0, r4
+                @dcw $F800
+                b merge
+            bow:
+                @align 4
+                ldr r0, [Adr+12]
+                mov lr, r0
+                mov r0, r4
+                @dcw $F800
+                b merge
+            magic:
+                @align 4
+                ldr r0, [Adr+16]
+                mov lr, r0
+                mov r0, r4
+                @dcw $F800
+            merge:
+                cmp r0, #0
+                beq end
+                
+                mov r0, #1
+            pop {r4, r5, pc}
+            end:
+                mov r0, #0
+            pop {r4, r5, pc}
     
     
 @ltorg
