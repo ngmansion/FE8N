5331f205fd5d3e59147db9e34d9656d3d4f2ed80
ngmansion <eliwooood@gmail.com>
2018-01-08 22:04:31 +0900

    スキル確率変動
    必殺率まで上がっていたバグを修正

--- Engine Hacks/_N/_Skills/NihilKing/mikiri.asm
+++ Engine Hacks/_N/_Skills/NihilKing/mikiri.asm
@@ -5,25 +5,27 @@
 	lsr	r3, r0, #16
 	lsl	r1, r1, #24
 	asr	r2, r1, #24
-	ldr	r0, =$0203A4D0
+	ldr r0, =$0802a4b4
+	ldr	r0, [r0]
 	ldrh	r1, [r0, #0]
 	mov	r0, #2
 	and	r0, r1
-	beq	start
+	beq	start ;独自処理
 	mov	r0, r2
 	bx	lr
 start:
+    mov r0, lr ;戻りアドレス確認
 ;除外判定
-	cmp	r2, #0
-	bne	return
-	ldr	r1, =$0802b40B	;;必殺は除外
-	cmp	r0, r1
-	beq	return
-	ldr	r1, =$0802b607	;;デビルアクスも除外
-	cmp	r0, r1
-	beq	return
+    cmp r2, #0
+    bne return ;r2は謎。通常は0が入る筈。気味が悪いから0なら除外
+    ldr r1, =$0802b40B ;必殺は対象外
+    cmp r0, r1
+    beq return
+    ldr r1, =$0802b607	;デビルアクスも対象外
+    cmp r0, r1
+    beq return
 ;除外判定終了
-	push	{r3}    ;;;;;;;;
+	push	{r3, lr}    ;;;;;;;;
 	mov	r3, sp
 	ldr r2, =$0802AFD1 ;汎用
 	ldr r1, =$0802aff9 ;連続専用
@@ -68,56 +70,55 @@ Reverse
 	eor r2, r3
 	eor r3, r2
 nonTATE
-    push {lr}    ;;;;;;;;
 ;見切りチェック
-    push {r2}
     mov r0, r3
+        push {r2}
         @align 4
         ldr r3, [adr]
         mov lr, r3
         @dcw $F800
-    pop {r2}
+        pop {r2}
     cmp r0, #0
     beq toking
     mov r0, #0
     str r0, [sp+4] ;r3
     
 toking: ;王の器チェック
-    push {r2}
     mov r0, r2
+        push {r2}
         @align 4
         ldr r3, [adr+4] ;王の器
         mov lr, r3
         @dcw $F800
-    pop {r2}
+        pop {r2}
     cmp r0, #0
     beq togod
-    ldr r0, [sp+4] ;r3
+    ldr r0, [sp] ;r3
     add r0, #10
-    str r0, [sp+4] ;r3
+    str r0, [sp] ;r3
     
 togod: ;神の器チェック
-    push {r2}
     mov r0, r2
+        push {r2}
         @align 4
         ldr r3, [adr+12] ;神の器
         mov lr, r3
         @dcw $F800
-    pop {r2}
+        pop {r2}
     cmp r0, #0
     beq toace
-    ldr r0, [sp+4] ;r3
+    ldr r0, [sp] ;r3
     add r0, #30
-    str r0, [sp+4] ;r3
+    str r0, [sp] ;r3
     
 toace: ;勇将チェック
-    push {r2}
     mov r0, r2
+        push {r2}
         @align 4
         ldr r3, [adr+8] ;勇将
         mov lr, r3
         @dcw $F800
-    pop {r2}
+        pop {r2}
 	cmp	r0, #0
 	beq	end
 gotAC:
@@ -126,15 +127,14 @@ gotAC:
 	lsl	r0, r0, #1
 	cmp	r0, r1
 	bgt	end
-	ldr	r0, [sp+4] ;r3
+	ldr	r0, [sp] ;r3
 	add	r0, #30
-	str	r0, [sp+4] ;r3
+	str	r0, [sp] ;r3
 end
     mov r2, #0
-    pop {r0}
-    mov lr, r0 ;r14復活
     pop {r3}
-    b return
+    ldr r0, =RETURN_ADR
+    mov pc, r0
 
 ;non
 ;	pop	{r3}
