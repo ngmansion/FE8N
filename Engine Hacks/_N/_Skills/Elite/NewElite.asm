4ac32a6ba2b6a081f5f121f4be9028c25037ec72
ngmansion <eliwooood@gmail.com>
2018-01-11 21:37:43 +0900

    スキル「エリート」
    怪しい？ところを修正

--- Engine Hacks/_N/_Skills/Elite/NewElite.asm
+++ Engine Hacks/_N/_Skills/Elite/NewElite.asm
@@ -1,24 +1,22 @@
 @thumb
-	push	{lr}
-	push {r0}
-
-	@align 4
-	ldr r1, [adr] ;
-	mov lr, r1
-	@dcw $F800
-	
-	mov r1, r0
-	pop {r0}
-	cmp r1, #0
-	bne elite
-	ldr	r1, [r0]
-	ldr	r2, [r0, #4]
-	ldr	r0, [r1, #0x28]
-	ldr	r1, [r2, #0x28]
-	ldr	r3, =$0802C35E
-	mov	pc, r3
+    push {lr}
+      push {r0}
+        @align 4
+        ldr r1, [adr] ;
+        mov lr, r1
+        @dcw $F800
+        mov r1, r0
+      pop {r0}
+    cmp r1, #0
+    bne elite
+    ldr r1, [r0]
+    ldr r2, [r0, #4]
+    ldr r0, [r1, #40]
+    ldr r1, [r2, #40]
+    ldr r3, =$0802C35E
+    mov pc, r3
 elite
-	ldr	r3, =$0802C36C
-	mov	pc, r3
+    ldr r3, =$0802C36C
+    mov pc, r3
 @ltorg
 adr:
