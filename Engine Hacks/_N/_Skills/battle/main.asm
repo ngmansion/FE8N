b8c7e1ddfdb4d52f81ba34c4fe25a167e55268b0
ngmansion <eliwooood@gmail.com>
2018-01-26 22:46:07 +0900

    殺し強化

--- Engine Hacks/_N/_Skills/battle/main.asm
+++ Engine Hacks/_N/_Skills/battle/main.asm
@@ -23,18 +23,45 @@
     
     bl kongou
     
+    bl koroshi
+    
 Return:
     ldr r5, [r4, #76]
     ldr r0, =$0802ad44
     mov r15, r0
-    
-
 true:
     mov r0, #1
     pop {pc}
 false:
     mov r0, #0
     pop {pc}
+
+koroshi:
+    push {lr}
+    bl breaker_impl
+    cmp r0, #0
+    beq false
+    
+    mov r1, #90
+    ldrh r0, [r4, r1]
+    add r0, #3
+    strh r0, [r4, r1] ;自分
+    
+    mov r1, #92
+    ldrh r0, [r4, r1]
+    add r0, #3
+    strh r0, [r4, r1] ;自分
+    
+    mov r1, #94
+    ldrh r0, [r4, r1]
+    add r0, #20
+    strh r0, [r4, r1] ;自分
+    
+    mov r1, #96
+    ldrh r0, [r4, r1]
+    add r0, #20
+    strh r0, [r4, r1] ;自分
+    b true
 
 shisen:
     push {lr}
@@ -46,18 +73,13 @@ shisen:
     cmp r0, #0
     beq false
     
-    mov r1, r4
-    mov r0, #90
-    ldrh r0, [r1, r0]
+    mov r1, #90
+    ldrh r0, [r4, r1]
     add r0, #10
-    add r1, #90
-    strh r0, [r1] ;自分
-    mov r1, r6
-    mov r0, #90
-    ldrh r0, [r1, r0]
+    strh r0, [r4, r1] ;自分
+    ldrh r0, [r6, r1]
     add r0, #10
-    add r1, #90
-    strh r0, [r1] ;相手
+    strh r0, [r6, r1] ;相手
     b true
 
 kishin:
@@ -77,19 +99,15 @@ kishin:
     ldrb r1, [r4, #0xB]
     cmp r0, r1
     bne false
-    mov r1, r4
-    mov r0, #90
-    ldrh r0, [r1, r0]
+    mov r1, #90
+    ldrh r0, [r4, r1]
     add r0, #5 ;威力
-    add r1, #90
-    strh r0, [r1] ;自分
+    strh r0, [r4, r1] ;自分
 
-    mov r1, r4
-    mov r0, #106
-    ldrh r0, [r1, r0]
+    mov r1, #106
+    ldrh r0, [r4, r1]
     add r0, #15 ;必殺
-    add r1, #106
-    strh r0, [r1] ;自分
+    strh r0, [r4, r1] ;自分
     b true
 
 kongou:
@@ -109,14 +127,70 @@ kongou:
     ldrb r1, [r4, #0xB]
     cmp r0, r1
     bne false
-    mov r1, r6
-    mov r0, #90
-    ldrh r0, [r1, r0]
-    sub r0, #10
-    add r1, #90
-    strh r0, [r1] ;相手
+    mov r1, #92
+    ldrh r0, [r4, r1]
+    add r0, #10
+    strh r0, [r4, r1] ;自分
     b true
-
+breaker_impl:
+    mov r0, r4
+    add r0, #74
+    ldrh r0, [r0]
+        ldr r1, =$080172f0
+        mov lr, r1
+        @dcw $F800
+    cmp r0, #0
+    beq sword
+    cmp r0, #1
+    beq lance
+    cmp r0, #2
+    beq axe
+    cmp r0, #3
+    beq bow
+    cmp r0, #4
+    beq false
+    cmp r0, #7
+    ble magic
+    b false
+sword:
+    @align 4
+    ldr r0, [adr+16]
+    mov lr, r0
+    mov r0, r4
+    @dcw $F800
+    b merge
+lance:
+    @align 4
+    ldr r0, [adr+20]
+    mov lr, r0
+    mov r0, r4
+    @dcw $F800
+    b merge
+axe:
+    @align 4
+    ldr r0, [adr+24]
+    mov lr, r0
+    mov r0, r4
+    @dcw $F800
+    b merge
+bow:
+    @align 4
+    ldr r0, [adr+28]
+    mov lr, r0
+    mov r0, r4
+    @dcw $F800
+    b merge
+magic:
+    @align 4
+    ldr r0, [adr+32]
+    mov lr, r0
+    mov r0, r4
+    @dcw $F800
+merge:
+    cmp r0, #0
+    @dcw $D000
+    b true
+    pop {pc}
 @ltorg
 adr:
 
