9322800b9fab3d1c41e88b285b5f42e45eb447b8
ngmansion <eliwooood@gmail.com>
2018-02-07 00:27:45 +0900

    スキル「治癒」

---
+++ Engine Hacks/_N/_Skills/Kaifuku/tiyu.asm
@@ -0,0 +1,19 @@
+@thumb
+    push {r4, lr}
+    mov r4, r0
+    mov r0, r5
+        @align 4
+        ldr r1, [adr] ;‰ñ•œ
+        mov lr, r1
+    mov r2, r0
+    ldr r1, =$08019f44
+    ldr r1, [r1]
+    add r0, r0, r1
+    ldrb r0, [r0, #0]
+    lsl r0, r0, #24
+    asr r0, r0, #24
+    orr r0, r2
+    pop {r4, pc}
+    bx lr
+@ltorg
+adr:
