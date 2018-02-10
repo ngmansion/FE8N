1e45c1e0fe34d3df1b7e277476f65e1d9b4137d2
ngmansion <eliwooood@gmail.com>
2018-02-10 15:07:55 +0900

    奥義の書

--- Engine Hacks/_N/_Skills/Kantsuu/Third.asm
+++ Engine Hacks/_N/_Skills/Kantsuu/Third.asm
@@ -188,16 +188,14 @@ retrun
 	
 Gecko:
 	push	{lr}
-	mov	r0, #72
-	ldrh	r0, [r7, r0]
-		bl	WEAPON
-	cmp	r0, #7
-	bgt	false
-	add	r0, #40
-	ldrb	r0, [r7, r0]
-	cmp	r0, #250
-	bls	false
-	
+    mov r0, r7
+        @align 4
+        ldr r1, [adr+24] ;奥義判定
+        mov lr, r1
+        @dcw $F800
+    cmp r0, #0
+    beq false ;奥義無し
+
 	ldrb	r0, [r7, #0x15]	;;奥義発動率
 	mov	r1, #0
 	ldr	r2, =$0802a490 ;;r0=確率, r1=#0 乱数
