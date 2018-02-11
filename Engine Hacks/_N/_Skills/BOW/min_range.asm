ef7b25aa2edce6efc50a04aac882735385693013
ngmansion <eliwooood@gmail.com>
2018-02-12 00:32:31 +0900

    BOW Kantsu soltraether
    諸々バグ修正

--- Engine Hacks/_N/_Skills/BOW/min_range.asm
+++ Engine Hacks/_N/_Skills/BOW/min_range.asm
@@ -2,47 +2,49 @@
 ;@org	$08017420
 
 ;▽元処理
-	ldr 	r0, =$080172d4	;アイテムテーブル(説明ID)
-	ldr 	r0, [r0]
-	add	r1, r1, r0
-	ldrb	r0, [r1, #25]
+    ldr r0, =$080172d4	;アイテムテーブル(説明ID)
+    ldr r0, [r0]
+    add r1, r1, r0
+    ldrb r0, [r1, #25]
 ;△元処理
-	mov	r1, r13
-	ldr	r2, =$03007D6C
-	cmp	r1, r2
-	beq	end
-	ldr	r1, [r1, #4]
-	ldr	r2, =$0802A833
-	cmp	r1, r2
-	beq	five
-	mov	r1, #0x1
-	lsl	r1, r1, #16
-	cmp	r6, r1
-	bgt	six
-	cmp	r5, r1
-	blt	zero
-five:
-	ldr	r1, [r5, #4]
-	b	kit
-six:
-	ldr	r1, [r6, #4]
-	b	kit
-zero:
-	ldr	r1, =$03004df0
-	ldr	r1, [r1]
-	cmp	r1, #0
-	beq	end
-	ldr	r1, [r1, #4]
-kit:
-	ldrb	r1, [r1, #4]
-	cmp	r1, #0x1D		;フォレストナイト
-	beq	tikai
-	cmp	r1, #0x1E		;フォレストナイト
-	bne	end
-tikai:
-	cmp	r0, #0x22
-	bne	end
-	sub	r0, #0x10
+    mov r1, r13
+    ldr r2, =$03007D6C
+    cmp r1, r2
+    beq end
+    ldr r1, [r1, #4]
+    ldr r2, =$0802A833
+    cmp r1, r2
+    beq five
+    mov r1, #0x1
+    lsl r1, r1, #16
+    cmp r6, r1
+    bgt six
+    cmp r5, r1
+    blt zero
+five
+    ldr r1, [r5, #4]
+    b kit
+six
+    ldr r1, [r6, #4]
+    b kit
+zero
+    ldr r1, =$03004df0
+    ldr r1, [r1]
+    cmp r1, #0
+    beq end
+    ldr r1, [r1, #4]
+kit
+    ldrb r1, [r1, #4]
+    cmp r1, #0x1D		;フォレストナイト
+    beq tikai
+    cmp r1, #0x1E		;フォレストナイト
+    bne end
+tikai
+    cmp r0, #0x11
+    beq end
+    cmp r0, #0x12
+    beq end
+    sub r0, #0x10
 end:
-	lsr	r0, r0, #4
-	bx	lr
+    lsr r0, r0, #4
+    bx lr
--- Engine Hacks/_N/_Skills/BOW/select_player_func.asm
+++ Engine Hacks/_N/_Skills/BOW/select_player_func.asm
@@ -1,49 +1,52 @@
 @thumb
-;@org	$08016e7c
-	push	{lr}
-	mov		r1, #255
-	and		r0, r1
-	lsl		r1, r0, #3
-	add		r1, r1, r0
-	lsl		r1, r1, #2
-	ldr 	r0, =$080172d4	;アイテムテーブル(説明ID)
-	ldr 	r0, [r0]
-	add		r1, r1, r0
-	ldrb	r0, [r1, #25]		;射程ロード
+;@org $08016e7c
+    push {lr}
+    mov r1, #255
+    and r0, r1
+    lsl r1, r0, #3
+    add r1, r1, r0
+    lsl r1, r1, #2
+    ldr r0, =$080172d4	;アイテムテーブル(説明ID)
+    ldr r0, [r0]
+    add r1, r1, r0
+    ldrb r0, [r1, #25]		;射程ロード
 ;;;;;▽撃破捕獲追加処理▽
-	ldr		r1, [r6, #12]	;捕獲
-	mov		r3,#0x80
-	lsl		r3, r3,#0x17
-	tst		r1, r3
-	bne	one
+    ldr r1, [r6, #12]	;捕獲
+    mov r3,#0x80
+    lsl r3, r3,#0x17
+    tst r1, r3
+    bne one
 ;;;;;△撃破捕獲追加処理△
-	ldr		r1, [r6, #4]
-	ldrb	r1, [r1, #4]
-	cmp		r1, #0x1B
-	beq		magi
-	cmp		r1, #0x1C
-	beq		magi
-	cmp		r1, #0x1D
-	beq		tikai
-	cmp		r1, #0x1E
-	beq		tikai
-	b		end
-;捕獲フラグがオン
-one:
-	mov		r0, #0x11
-	b		end
-;神射手
-magi
-	cmp		r0, #0x22
-	bne		end
-	add		r0, #1
-	b		end
-;近接射撃
-tikai:
-	lsr		r0, #0x22
-	bne		end
-	sub		r0, #0x10
-	b		end
-end:
-	ldr	r3, =$08016e8e
-	mov	pc, r3
+    ldr r1, [r6, #4]
+    ldrb r1, [r1, #4]
+    cmp r1, #0x1B
+    beq magi
+    cmp r1, #0x1C
+    beq magi
+    cmp r1, #0x1D
+    beq tikai
+    cmp r1, #0x1E
+    beq tikai
+    b end
+one	;捕獲フラグがオン
+    mov r0, #0x11
+    b end
+magi	;神射手
+    cmp r0, #0x22
+    bne end
+    add r0, #1
+    b end
+tikai ;近接射撃
+    cmp r0, #0x11
+    beq end
+    cmp r0, #0x12
+    beq end
+    sub r0, #0x10
+    b end
+end
+    ldr	r3, =$08016e8e
+    mov	pc, r3
+
+
+
+
