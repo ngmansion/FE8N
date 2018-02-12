886847d3ead0541f578756e1b079603e628cad6b
ngmansion <eliwooood@gmail.com>
2018-02-13 00:40:21 +0900

    Tokkou
    ダメージ計算がおかしいバグ修正

--- Engine Hacks/_N/_Skills/Tokkou/weapon.asm
+++ Engine Hacks/_N/_Skills/Tokkou/weapon.asm
@@ -4,16 +4,15 @@
     cmp r0, #0
     beq non
     
-    mov	r0, #90
+    mov r0, #90
     ldsh r0, [r6, r0]
         @align 4
         ldr r1, [adr]
     mul r0, r1
-non
-    add r0, r0, r5
-    
     lsl r0, r0, #16
     asr r4, r0, #16
+non
+    add r4, r4, r5
     b $0802aab6
 @align 4
 adr
