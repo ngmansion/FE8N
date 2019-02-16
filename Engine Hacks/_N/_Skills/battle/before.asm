ff170f6e21196a9f168f1ea03ab0c6e61d24a1b9
aera <eliwooood@gmail.com>
2019-02-16 22:13:27 +0900

    魔道の刃とか

--- Engine Hacks/_N/_Skills/battle/before.asm
+++ Engine Hacks/_N/_Skills/battle/before.asm
@@ -1,23 +1,43 @@
-;0x02ad3c
-;ステータス画面にも影響がある
-;相手が存在するとは限らない(ダミーかもしれない)
-@thumb
+.equ SAVIOR_ADR, (adr+44)
+.equ SAVIOR_DAMAGE, (10)
+
+.macro _blh to, reg=r3
+	ldr \reg, =\to
+	mov lr, \reg
+	.short 0xF800
+.endm
+
+.macro _bldr reg, dest
+	ldr \reg, =\dest
+	mov lr, \reg
+	.short 0xF800
+.endm
+
+.macro _blr reg
+	mov lr, \reg
+	.short 0xF800
+.endm
+
+@;0x02a90c
+@;ステータス画面にも影響がある
+@;相手が存在するとは限らない(ダミーかもしれない)
+.thumb
     mov	r0, r5
-        @align 4
-        ldr r1, [adr] ;見切り
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr	@;見切り
+    _blr r1
     cmp r0, #0
     bne RETURN
-;闘技場チェック
-    ldr r0, =$0203a4d0
+@;闘技場チェック
+    ldr r0, Alina_Adr
     ldrh r0, [r0]
     mov r1, #0x20
     and r0, r1
     bne RETURN
     
     bl shishi
-;相手の存在をチェック
+    
+    bl Savior	@;護り手
+@;相手の存在をチェック
     ldr r0, [r5, #4]
     cmp r0, #0
     beq RETURN
@@ -38,26 +58,48 @@ RETURN:
     bx r0
 
 
+    Savior:
+        push {lr}
+        
+        mov r0, r4
+        ldr r1, SAVIOR_ADR
+        _blr r1
+        cmp r0, #0
+        beq endSavior
+        
+        ldr r0, [r4, #12]
+        mov r1, #0x10
+        and r0, r1
+        beq endSavior
+        
+        mov r1, #92
+        ldrh r0, [r4, r1]
+        add r0, #SAVIOR_DAMAGE
+        strh r0, [r4, r1]
+        mov r0, #1
+        .short 0xE000
+    endSavior:
+        mov r0, #0
+        pop {pc}
+
     DistantDef:
         push {lr}
-        ldr r0, =$0203a4d2
-        ldrb r0, [r0] ;距離
+        ldr r0, Range_Adr
+        ldrb r0, [r0] @;距離
         cmp r0, #1
         ble endDistantDef
         
         mov r0, r4
         ldrb r0, [r0, #11]
-        ldr r1, =$03004df0
+        ldr r1, Attacker_Adr
         ldr r1, [r1]
         ldrb r1, [r1, #11]
         cmp r0, r1
-        beq endDistantDef	;攻撃者と一致
+        beq endDistantDef	@;攻撃者と一致
         
         mov r0, r4
-            @align 4
-            ldr r1, [adr+40] ;遠距離防御
-            mov lr, r1
-            @dcw $F800
+        ldr r1, adr+40	@;遠距離防御
+        _blr r1
         cmp r0, #0
         beq endDistantDef
         
@@ -66,32 +108,30 @@ RETURN:
         add r0, #6
         strh r0, [r4, r1]
         mov r0, #1
-        @dcw $E000
-    endDistantDef
+        .short 0xE000
+    endDistantDef:
         mov r0, #0
         pop {pc}
         
 
     CloseDef:
         push {lr}
-        ldr r0, =$0203a4d2
-        ldrb r0, [r0] ;距離
+        ldr r0, Range_Adr
+        ldrb r0, [r0] @;距離
         cmp r0, #1
         bne endCloseDef
         
         mov r0, r4
         ldrb r0, [r0, #11]
-        ldr r1, =$03004df0
+        ldr r1, Attacker_Adr
         ldr r1, [r1]
         ldrb r1, [r1, #11]
         cmp r0, r1
-        beq endCloseDef	;攻撃者と一致
+        beq endCloseDef	@;攻撃者と一致
         
         mov r0, r4
-            @align 4
-            ldr r1, [adr+36] ;近距離防御
-            mov lr, r1
-            @dcw $F800
+        ldr r1, adr+36 @;近距離防御
+        _blr r1
         cmp r0, #0
         beq endCloseDef
         
@@ -100,8 +140,8 @@ RETURN:
         add r0, #6
         strh r0, [r4, r1]
         mov r0, #1
-        @dcw $E000
-    endCloseDef
+        .short 0xE000
+    endCloseDef:
         mov r0, #0
         pop {pc}
 
@@ -117,56 +157,52 @@ koroshi:
     bl breaker_impl
     cmp r0, #0
     beq false
-gotKoroshi
+gotKoroshi:
     mov r1, #90
     ldrh r0, [r4, r1]
     add r0, #3
-    strh r0, [r4, r1] ;自分
+    strh r0, [r4, r1] @;自分
     
     mov r1, #92
     ldrh r0, [r4, r1]
     add r0, #3
-    strh r0, [r4, r1] ;自分
+    strh r0, [r4, r1] @;自分
     
     mov r1, #94
     ldrh r0, [r4, r1]
     add r0, #3
-    strh r0, [r4, r1] ;自分
+    strh r0, [r4, r1] @;自分
     
     mov r1, #96
     ldrh r0, [r4, r1]
     add r0, #20
-    strh r0, [r4, r1] ;自分
+    strh r0, [r4, r1] @;自分
     
     mov r1, #98
     ldrh r0, [r4, r1]
     add r0, #20
-    strh r0, [r4, r1] ;自分
+    strh r0, [r4, r1] @;自分
     b true
 
 shishi:
     push {lr}
     mov r0, r4
-        @align 4
-        ldr r1, [adr+4]
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+4
+    _blr r1
     cmp r0, #0
-    @dcw $D000
+    .short 0xD000
     b gotKoroshi
     b false
     
 kishin:
     push {lr}
     mov r0, r4
-        @align 4
-        ldr r1, [adr+8] ;鬼神
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+8	@;鬼神
+    _blr r1
     cmp r0, #0
     beq false
     
-    ldr r0, =$03004df0
+    ldr r0, Attacker_Adr
     ldr r0, [r0]
     ldrb r0, [r0, #0xB]
     
@@ -175,26 +211,24 @@ kishin:
     bne false
     mov r1, #90
     ldrh r0, [r4, r1]
-    add r0, #5 ;威力
-    strh r0, [r4, r1] ;自分
+    add r0, #5 @;威力
+    strh r0, [r4, r1] @;自分
 
     mov r1, #102
     ldrh r0, [r4, r1]
-    add r0, #15 ;必殺
-    strh r0, [r4, r1] ;自分
+    add r0, #15 @;必殺
+    strh r0, [r4, r1] @;自分
     b true
 
 kongou:
     push {lr}
     mov r0, r4
-        @align 4
-        ldr r1, [adr+12] ;金剛
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+12 @;金剛
+    _blr r1
     cmp r0, #0
     beq false
     
-    ldr r0, =$03004df0
+    ldr r0, Attacker_Adr
     ldr r0, [r0]
     ldrb r0, [r0, #0xB]
     
@@ -212,9 +246,8 @@ breaker_impl:
     mov r0, r5
     add r0, #74
     ldrh r0, [r0]
-        ldr r1, =$080172f0
-        mov lr, r1
-        @dcw $F800
+    ldr r1, Equipment_Adr
+    _blr r1
     cmp r0, #0
     beq sword
     cmp r0, #1
@@ -229,42 +262,40 @@ breaker_impl:
     ble magic
     b false
 sword:
-    @align 4
-    ldr r0, [adr+16]
-    mov lr, r0
     mov r0, r4
-    @dcw $F800
+    ldr r1, adr+16
+    _blr r1
     b merge
 lance:
-    @align 4
-    ldr r0, [adr+20]
-    mov lr, r0
     mov r0, r4
-    @dcw $F800
+    ldr r1, adr+20
+    _blr r1
     b merge
 axe:
-    @align 4
-    ldr r0, [adr+24]
-    mov lr, r0
     mov r0, r4
-    @dcw $F800
+    ldr r1, adr+24
+    _blr r1
     b merge
 bow:
-    @align 4
-    ldr r0, [adr+28]
-    mov lr, r0
     mov r0, r4
-    @dcw $F800
+    ldr r1, adr+28
+    _blr r1
     b merge
 magic:
-    @align 4
-    ldr r0, [adr+32]
-    mov lr, r0
     mov r0, r4
-    @dcw $F800
+    ldr r1, adr+32
+    _blr r1
 merge:
     pop {pc}
-
-@ltorg
+.align
+Alina_Adr:
+.long 0x0203a4d0
+Range_Adr:
+.long 0x0203a4d2
+Attacker_Adr:
+.long 0x03004df0
+Equipment_Adr:
+.long 0x080172f0
+.ltorg
 adr:
 
--- Engine Hacks/_N/_Skills/battle/main.asm
+++ Engine Hacks/_N/_Skills/battle/main.asm
@@ -1,5 +1,27 @@
-;0x02ad3c
-@thumb
+.thumb
+
+@;0x02ad3c
+@;イクリプス等の直前(自分の数値と相手の数値の計算後)
+@;ステータス画面では呼ばれない
+@;相手の数値に影響を与える処理群
+.macro _blh to, reg=r3
+	ldr \reg, =\to
+	mov lr, \reg
+	.short 0xF800
+.endm
+
+.macro _bldr reg, dest
+	ldr \reg, =\dest
+	mov lr, \reg
+	.short 0xF800
+.endm
+
+.macro _blr reg
+	mov lr, \reg
+	.short 0xF800
+.endm
+
+
     push {r4, r5, r6, lr}
     mov r4, r0
     mov r6, r1
@@ -9,10 +31,8 @@
     bne endZero
     
     mov	r0, r6
-        @align 4
-        ldr r1, [adr+16] ;見切り
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+16 @;見切り
+    _blr r1
     cmp r0, #0
     bne next
     
@@ -24,18 +44,18 @@ next:
 
 Return:
     ldr r5, [r4, #76]
-    ldr r0, =$0802ad44
+    ldr r0, =0x0802ad44
     mov r15, r0
 endZero:
     mov r0, #0
     mov r1, r4
     add r1, #90
-    strh r0, [r1] ;威力
+    strh r0, [r1] @;威力
     
     mov r0, #0x7F
     mov r1, r6
     add r1, #92
-    strh r0, [r1] ;防御
+    strh r0, [r1] @;防御
     pop {r4, r5, r6}
     pop {r0}
     bx r0
@@ -45,20 +65,18 @@ endZero:
 DistantGuard:
     push {lr}
     mov r0, r6
-        @align 4
-        ldr r1, [adr+0] ;遠距離無効
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+0 @;遠距離無効
+    _blr r1
     cmp r0, #0
     beq endDistantGuard
     
-    ldr r0, =$0203a4d2
-    ldrb r0, [r0] ;距離
+    ldr r0, =0x0203a4d2
+    ldrb r0, [r0] @;距離
     cmp r0, #1
     beq endDistantGuard
     
     mov r0, #1
-    @dcw $E000
+    .short 0xE000
 endDistantGuard:
     mov r0, #0
     pop {pc}
@@ -66,28 +84,24 @@ endDistantGuard:
 godBless:
     push {lr}
     mov r0, r4
-        @align 4
-        ldr r1, [adr+4] ;光の加護
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+4 @;光の加護
+    _blr r1
     cmp r0, #0
     bne endBless
     
     mov r0, r6
-        @align 4
-        ldr r1, [adr+8] ;暗黒の加護
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+8 @;暗黒の加護
+    _blr r1
     cmp r0, #0
     beq endBless
 
     mov r1, #90
-    ldrh r0, [r4, r1] ;威力
+    ldrh r0, [r4, r1] @;威力
     asr r0, r0, #1
-    strh r0, [r4, r1] ;威力
+    strh r0, [r4, r1] @;威力
 
     mov r0, #1
-    @dcw $E000
+    .short 0xE000
 endBless:
     mov r0, #0
     pop {pc}
@@ -95,10 +109,8 @@ endBless:
 shisen:
     push {lr}
     mov r0, r4
-        @align 4
-        ldr r1, [adr+12] ;死線
-        mov lr, r1
-        @dcw $F800
+    ldr r1, adr+12 @;死線
+    _blr r1
     cmp r0, #0
     beq false
     
@@ -107,14 +119,14 @@ shisen:
     ldrh r0, [r1, r0]
     add r0, #10
     add r1, #90
-    strh r0, [r1] ;自分
+    strh r0, [r1] @;自分
     
     mov r1, r6
     mov r0, #90
     ldrh r0, [r1, r0]
     add r0, #10
     add r1, #90
-    strh r0, [r1] ;相手
+    strh r0, [r1] @;相手
     b true
 
 true:
@@ -124,6 +136,6 @@ false:
     mov r0, #0
     pop {pc}
 
-@ltorg
+.ltorg
 adr:
 
