abe20bb8e8dfcaeaaca29af830eb6e92a691d9df
7743 <jmp.ebx@gmail.com>
2019-02-19 17:41:13 +0900

    防衛スキルが発動しないバグ修正

--- Engine Hacks/_N/_Skills/_anime/get_skillanime_data.s
+++ Engine Hacks/_N/_Skills/_anime/get_skillanime_data.s
@@ -4,40 +4,29 @@
 @
 @引数 r0=スキルの種類 #0x00=攻撃 #0x01=防衛
 .thumb
-    push {r4,lr}
+    push {lr}
 
-	mov		r4,r0				@スキルの分類
+	ldr r3, =0x0203AB40		@発動したスキルアニメを記録している場所
+							@Player 攻撃スキル	0203AB40
+							@Player 防衛スキル	0203AB41
+							@Enemy  攻撃スキル	0203AB42
+							@Enemy  防衛スキル	0203AB43
+							@攻撃スキルと防衛スキルが同時に発動することがあるので分ける.
 
-	ldr		r3, =0x0203AB40  @保持していると判定されたスキルIDを記録. スキル発動のアニメーションを出す時に利用する
-
-	ldr		r0, =0x0203E188		@戦闘アニメで攻撃側へのRAMポインタ
-	ldr		r0, [r0, #0x0]		@RAMデータへ
-
-	mov		r2,#0xB
-	ldrb	r2,[r0,r2]	@UnitRAMPointer->部隊表ID	友軍+0x40	敵軍+0x80
-	cmp		r2,#0x80
-	blt		Player		@Player or Ally
+	LDR r1, =0x02029000
+	CMP r7, r1
+	BLS Player
 
 Enemy:
-	add		r3, #0x02		@敵軍
-	@b		Join1
+	add		r3, #0x02       @敵軍
+	add		r3, r0          @防衛スキルなら+1 攻撃スキルなら+0
+	b		Join1
 
 Player:
-	@nop
-@Join1:
+	add		r3, r0          @防衛スキルなら+1 攻撃スキルなら+0
+@	b		Join1
 
-	cmp		r4, #0x00		@スキルの分類を評価
-	beq		AttackSkill
-	b		DefenseSkill
-
-DefenseSkill:
-	add		r3,#0x01	@防衛スキルは+1する.
-	@b		Join2
-
-AttackSkill:
-	@nop
-
-@Join2:
+Join1:
 	ldrb	r0, [r3]      @記録しておいたスキルIDを取得
 
 	cmp		r0,#0x00
@@ -46,11 +35,9 @@ AttackSkill:
 	ldr		r1,	adr       @SkillAnimation* SkillAnimation[SKILL_ID] skillanimation@
 	lsl		r0 ,r0 ,#0x2	@r1=skill_id << 2 (ポインタ参照するため)
 	ldr		r0,[r1,r0]      @skl_anime_table[skill_id].SkillAnime
-	@cmp		r0,#0x00
-	@beq		Exit        @不明 ディフォルトアニメをそのまま利用
-	
+
 Exit:
-    pop {r4,pc}
+    pop {pc}
 .align 4
 .ltorg
 adr:
