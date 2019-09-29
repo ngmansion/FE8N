dc40b0ada84fbf46b0fcc4c0f364b43edb655ce4
aera <eliwooood@gmail.com>
2019-09-29 09:43:58 +0900

    物理・杖属性アイテムは、使い切っても消えない

---
+++ Engine Hacks/_N/BreakedWeapon/main.asm
@@ -0,0 +1,18 @@
+.thumb
+	
+	lsl r1, r0, #28
+	bmi nonBreak
+	ldr r1, adr+4
+	add r2, r2, r1
+	cmp	r2, #255
+	bgt nonBreak
+check:
+	mov r1, #5
+	and r0, r1
+	beq adr+8
+nonBreak:
+	lsl	r0, r2, #16
+	lsr	r0, r0, #16
+	.short 0xE004
+adr:
+
