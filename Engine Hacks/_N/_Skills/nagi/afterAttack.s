2021f1332bf6703c273b0f8c0c03205da52a6862
aera <eliwooood@gmail.com>
2019-10-13 22:06:47 +0900

    戦技のヒット音設定

--- Engine Hacks/_N/_Skills/nagi/afterAttack.s
+++ Engine Hacks/_N/_Skills/nagi/afterAttack.s
@@ -2,7 +2,7 @@
 .equ STR_ADR, (67)	@書き込み先(AI1カウンタ)
 .equ WAR_FLAG, (0xFF)	@フラグ
 
-@;ORG 0x02b808
+@ORG 0x02b808
 .thumb
 
 @前処理
@@ -10,7 +10,7 @@
 	bne START
 	ldr	r1, =0x0203a604
 	ldr	r0, [r1, #0]
-	add	r0, #4	@;;スキルとかのアレをカウントアップ	
+	add	r0, #4	@スキルとかのアレをカウントアップ	
 	str	r0, [r1, #0]
 	mov r0, #0
 START:
@@ -20,7 +20,7 @@ START:
 	mov r0, #2
 	and r0, r1
 	cmp r0, #0
-	bne RETURN	@;戦闘予測時はスキップ
+	bne RETURN	@戦闘予測時はスキップ
 	
 	bl WarSkill
 	
@@ -34,7 +34,7 @@ RETURN:
 WarSkill:
 	push {lr}
 @	mov r3, r13
-@	ldr r3, [r3, #16]	@;r8が相手のアドレス
+@	ldr r3, [r3, #16]	@r8が相手のアドレス
 	
 	ldr r0, =0x03004df0
 	ldr r3, [r0]
@@ -45,40 +45,45 @@ WarSkill:
 	mov r0, #0xFE
 	strb r0, [r3, r1]
 @命中チェック
-	ldr	r1, =0x0203a604
-	ldr	r0, [r1, #0]
-	sub	r0, #4	@;;スキルとかのアレの前の状態を取る
-	ldr r0, [r0]
+	ldr	r2, =0x0203a604
+	ldr	r2, [r2]
+	sub	r2, #4	@スキルとかのアレの前の状態を取る
+	ldr r0, [r2]
 	
-	mov r1, #2	@;外れフラグ
-	and r0, r1
-	bne endWar	@;外れフラグオンでジャンプ
-
+	mov r1, #2	@外れフラグ
+	and r1, r0
+	bne endWar	@外れフラグオンでジャンプ
+	
+	mov r1, #0x80
+	lsl r1, r1, #7
+	orr r0, r1
+	str r0, [r2]
+	
 @武器は損処理
 	mov r3, #0
 	mov r1, r13
-	ldr r1, [r1, #16]	@;r8が相手のアドレス
-	add	r1, #72	@;装備中(74はトップアイテム)
+	ldr r1, [r1, #16]	@r8が相手のアドレス
+	add	r1, #72	@装備中(74はトップアイテム)
 	ldrh	r0, [r1]
 	lsr r2, r0, #8
-	beq breakWar	@;もう壊れている
+	beq breakWar	@もう壊れている
 loop_top_break:
 	bl func_break
 	mov r1, r13
-	ldr r1, [r1, #16]	@;r8が相手のアドレス
+	ldr r1, [r1, #16]	@r8が相手のアドレス
 	add	r1, #72
 	strh	r0, [r1]
 	lsr r2, r0, #8
 	beq breakWar
 	add r3, #1
-	cmp r3, #4		@;4回壊す(通常合わせて5減る)
+	cmp r3, #4		@4回壊す(通常合わせて5減る)
 	blt loop_top_break
 endWar:
 	pop {pc}
 	
 breakWar:
 	mov r1, r13
-	ldr r1, [r1, #16]	@;r8が相手のアドレス
+	ldr r1, [r1, #16]	@r8が相手のアドレス
 	add	r1, #125
 	mov	r0, #1
 	strb	r0, [r1, #0]
