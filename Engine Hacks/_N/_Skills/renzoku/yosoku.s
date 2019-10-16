d8a315cb22704604eda672644872dabac14e39de
aera <eliwooood@gmail.com>
2019-10-16 23:04:27 +0900

    s変更

--- Engine Hacks/_N/_Skills/renzoku/yosoku.s
+++ Engine Hacks/_N/_Skills/renzoku/yosoku.s
@@ -1,4 +1,4 @@
-@thumb
+.thumb
     push {r2, r3}
 
     ldr r0, [r4, #76]
@@ -7,37 +7,37 @@
     cmp r0, #0
     bne got
     mov r0, r4
-    ldr r1, =$0203a568
+    ldr r1, =0x0203a568
     cmp r0, r1
     bne non_change1
-    ldr r1, =$0203a4e8
+    ldr r1, =0x0203a4e8
 non_change1:
-        @align 4
-        ldr r3, [adr] ;bolt
+        ldr r3, adr @bolt
         mov lr, r3
-        @dcw $F800
+        .short 0xF800
     cmp r0, #0
     bne got
-    ldr r0, =$0203a568
+    ldr r0, =0x0203a568
     mov r1, r4
     cmp r0, r1
     bne non_change2
-    ldr r0, =$0203a4e8
+    ldr r0, =0x0203a4e8
 non_change2:
-        @align 4
-        ldr r3, [adr] ;bolt
+        ldr r3, adr @bolt
         mov lr, r3
-        @dcw $F800
+        .short 0xF800
     cmp r0, #0
     beq non
     
-got
-    ldr r0, =$0803678e ;continue
+got:
+    ldr r0, =0x0803678e @continue
     b end
 non:
-    ldr r0, =$0803679a ;end
+    ldr r0, =0x0803679a @end
 end:
     pop {r2, r3}
     mov pc, r0
-@ltorg
+.align
+.ltorg
 adr:
+
