635422582d390d25dc84f6309a79ed68480d54e8
aera <eliwooood@gmail.com>
2019-12-01 21:08:17 +0900

    飛燕の一撃 性能調整

---
+++ Engine Hacks/_N/_Skills/_common/checkNihil.s
@@ -0,0 +1,32 @@
+.thumb
+HAS_NIHIL_FUNC = (ADR+0)
+HAS_SKILL_FUNC = (ADR+4)
+@
+@r0 = skill_unit
+@r1 = nihil_unit
+@r2 = skill_id
+@
+    push {r4, r5, lr}
+    mov r4, r0
+    mov r5, r2
+    bl hasNihil
+    cmp r0, #1
+    beq false
+    mov r0, r4
+    mov r1, r5
+    bl judgeSkill
+ret:
+    pop {r4, r5, pc}
+false:
+    mov r0, #0
+    pop {r4, r5, pc}
+hasNihil:
+    ldr r3, HAS_NIHIL_FUNC
+    mov pc, r3
+judgeSkill:
+    ldr r3, HAS_SKILL_FUNC
+    mov pc, r3
+.align
+.ltorg
+ADR:
+
--- Engine Hacks/_N/_Skills/follow_up/main.s
+++ Engine Hacks/_N/_Skills/follow_up/main.s
@@ -8,39 +8,9 @@ HAS_DARTING_FUNC = (Adr+36)
     mov r1, #0x20
     and r0, r1
     bne normal @闘技場なら終了
-    
-    bl waryFighter_judgeActivate
-    push {r0}
+
     bl followup_skill
-    pop {r1}
-    
-    cmp r0, #0x00
-    beq	no_active
-    cmp r0, #0x11
-    beq	no_active
-@絶対追撃発動
-    cmp r1, #0
-    bne normal @1なら守備隊形発動
-    cmp r0, #0x01
-    beq active1
-    cmp r0, #0x10
-    beq active2
-    b normal
-active1:
-    ldr r0, =0x0802af56
-    mov pc, r0
-active2:
-    ldr r0, =0x0802af5c
-    mov pc, r0
 
-no_active:
-    cmp r1, #0
-    beq normal
-    ldr r0, =0x0802af80 @追撃無し
-    mov pc, r0
-
-
-result:
     cmp r0, r1
     bgt greater1
     blt greater2
@@ -68,6 +38,20 @@ greater2:
     bl cancel
     b normal
 
+active1:
+    ldr r0, =0x0802af56
+    mov pc, r0
+active2:
+    ldr r0, =0x0802af5c
+    mov pc, r0
+normal: @通常追撃判定
+    mov r0, r5
+    mov r1, #94
+    ldsh r2, [r6, r1]
+    ldsh r3, [r5, r1]
+    ldr r1, =0x0802af24
+    mov pc, r1
+
 cancel:
         add r0, #94
 	    ldrh r0, [r0]
@@ -80,7 +64,7 @@ cancel:
     endCancel:
         bx lr
 
-
+followup_skill:
         push {lr}
         push {r4}
         push {r7}
@@ -88,26 +72,36 @@ cancel:
         mov r7, #0
         bl waryFighter_judgeActivate    @守備隊形
         cmp r0, #0
-        beq jump1
+        beq jumpWaryFighter
     @守備隊形発動中
         sub r4, #1
         sub r7, #1
-    jump1:
+    jumpWaryFighter:
+
+        mov r0, r5
+        mov r1, r6
         bl BrashAssault @差し違え
         cmp r0, #0
-        beq jump2
+        beq jumpBrashAssault
         add r4, #1
-    jump2:
+    jumpBrashAssault:
+
+        mov r0, r6
+        mov r1, r5
         bl QuickRiposte @切り返し
         cmp r0, #0
-        beq jump2
+        beq jumpQuickRiposte
         add r7, #1
-    jump2:
+    jumpQuickRiposte:
+
+        mov r0, r5
+        mov r1, r6
         bl Impact   @瞬撃
         cmp r0, #0
-        beq jump3
+        beq jumpImpact
         sub r7, #1
-    jump3:
+    jumpImpact:
+
         mov r0, r4
         mov r1, r7
         pop {r7}
@@ -115,17 +109,7 @@ cancel:
         pop {pc}
 
 
-    
-normal: @通常追撃判定
-    mov r0, r5
-    mov r1, #94
-    ldsh r2, [r6, r1]
-    ldsh r3, [r5, r1]
-    ldr r1, =0x0802af24
-    mov pc, r1
-
-
-active_DartingBlow:
+Impact:
         push {r4, r5, lr}
         mov r4, r0
         mov r5, r1
@@ -144,13 +128,6 @@ active_DartingBlow:
         cmp r0, #0
         beq falseDarting
 
-        ldr r0, =0x03004df0
-        ldr r0, [r0]
-        ldrb r0, [r0, #0xB]
-        ldrb r1, [r4, #0xB]
-        cmp r0, r1
-        bne falseDarting    @攻め者と不一致
-
         mov r0, #1
         b endDarting
     falseDarting:
@@ -159,9 +136,8 @@ active_DartingBlow:
         pop {r4, r5, pc}
 
 
-
 waryFighter_judgeActivate:
-    push {r7, lr}
+        push {r7, lr}
         mov r0, r6
         mov r1, r5
         bl waryFighter_impl
@@ -171,29 +147,17 @@ waryFighter_judgeActivate:
         mov r1, r6
         bl waryFighter_impl
         orr r0, r7
-    pop {r7, pc}
+        pop {r7, pc}
         
 waryFighter_impl:
         push {r4, r5, lr}
         mov r4, r0
         mov r5, r1
         
-        mov r0, r5
-            ldr r1, Adr+20 @見切り
-            mov lr, r1
-            .short 0xF800
-        cmp r0, #1
-        beq	non_waryFighter_impl
-
-        mov r0, r5
-        mov r1, r4
-        bl active_DartingBlow
-        cmp r0, #1
-        beq non_waryFighter_impl    @相手が飛燕発動
-        
         mov r0, r4
-            ldr r1, Adr+24 @守備隊形
-            mov lr, r1
+        mov r1, r5
+            ldr r2, Adr+24 @守備隊形@見切り
+            mov lr, r2
             .short 0xF800
         cmp r0, #0
         beq	non_waryFighter_impl
@@ -204,98 +168,51 @@ waryFighter_impl:
             mov r0, #0
         pop {r4, r5, pc}
 
+BrashAssault:   @差し違え
+        push {r4, r5, lr}
+        mov r4, r0
+        mov r5, r1
 
-followup_skill:
-    push {r7, lr}
-        bl formation_judgeActivate @隊形スキル
-        mov r7, r0
-        bl breaker_judgeActivate @殺しスキル
-        orr r0, r7
-    pop {r7, pc}
+        mov r0, r4
+        mov r1, r5
+            ldr r2, Adr+28 @攻撃隊形@見切り
+            mov lr, r2
+            .short 0xF800
+        cmp r0, #0
+        beq	non_bold
 
-    formation_judgeActivate:
-        push {r7, lr}
-            mov r0, r5
-            mov r1, r6
-            bl boldFighter @攻撃隊形
-            mov r7, r0
-            
-            mov r0, r6
-            mov r1, r5
-            bl vengefulFighter @迎撃隊形
-            lsl r0, r0, #4
-            orr r0, r7
-        pop {r7, pc}
+        mov r0, #72
+        ldrh r0, [r4, r0]	@反撃されないなら不可
+        cmp r0, #0
+        beq non_bold
+        mov r0, #72
+        ldrh r0, [r5, r0]	@反撃されないなら不可
+        cmp r0, #0
+        beq non_bold
+        mov r0, #1
+        .short 0xE000
+    non_bold:
+        mov r0, #0
+        pop {r4, r5, pc}
 
+QuickRiposte: @切り返し
+        push {r4, r5, lr}
+        mov r4, r0
+        mov r5, r1
 
-        boldFighter: @攻撃隊形
-            push {r4, r5, lr}
-                mov r4, r0
-                mov r5, r1
-                
-                mov r0, r5
-                    ldr r1, Adr+20 @見切り
-                    mov lr, r1
-                    .short 0xF800
-                cmp r0, #1
-                beq	non_bold
-                
-                mov r0, r5
-                mov r1, r4
-                bl active_DartingBlow
-                cmp r0, #1
-                beq	non_bold
+        mov r0, r4
+        mov r1, r5
+            ldr r2, Adr+32 @切り返し@見切り
+            mov lr, r2
+            .short 0xF800
+        cmp r0, #0
+        beq	non_vengeful
 
-                mov r0, r4
-                    ldr r1, Adr+28 @攻撃隊形
-                    mov lr, r1
-                    .short 0xF800
-                cmp r0, #0
-                beq	non_bold
-
-                mov r0, #72
-                ldrh r0, [r4, r0]	@反撃されないなら不可
-                cmp r0, #0
-                beq non_bold
-                mov r0, #72
-                ldrh r0, [r5, r0]	@反撃されないなら不可
-                cmp r0, #0
-                beq non_bold
-                mov r0, #1
-                .short 0xE000
-            non_bold:
-                mov r0, #0
-            pop {r4, r5, pc}
-
-        vengefulFighter: @迎撃隊形
-            push {r4, r5, lr}
-                mov r4, r0
-                mov r5, r1
-                
-                mov r0, r5
-                    ldr r1, Adr+20 @見切り
-                    mov lr, r1
-                    .short 0xF800
-                cmp r0, #1
-                beq	non_vengeful
-                
-                mov r0, r5
-                mov r1, r4
-                bl active_DartingBlow
-                cmp r0, #1
-                beq	non_vengeful
-
-                mov r0, r4
-                    ldr r1, Adr+32 @迎撃隊形
-                    mov lr, r1
-                    .short 0xF800
-                cmp r0, #0
-                beq	non_vengeful
-                mov r0, #1
-                .short 0xE000
-            non_vengeful:
-                mov r0, #0
-            pop {r4, r5, pc}
+        mov r0, #1
+        .short 0xE000
+    non_vengeful:
+        mov r0, #0
+        pop {r4, r5, pc}
 
 
 
