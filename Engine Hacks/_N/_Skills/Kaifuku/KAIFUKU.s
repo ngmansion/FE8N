e2ec568557ef8570bd369060276469a653ff30c9
aera <eliwooood@gmail.com>
2019-12-07 12:44:58 +0900

    divine

--- Engine Hacks/_N/_Skills/Kaifuku/KAIFUKU.s
+++ Engine Hacks/_N/_Skills/Kaifuku/KAIFUKU.s
@@ -1,12 +1,12 @@
-@thumb
-;@org	$08019f28
+.thumb
+@org	$08019f28
     push {r4, lr}
     mov r4, r0
-    @align 4
-    ldr r1, [adr] ;回復
-    mov lr, r1
     mov r0, r5
-    @dcw $F800
+    mov r1, #0
+        ldr r2, adr @回復
+        mov lr, r2
+        .short 0xF800
     cmp r0, #0
     beq nonHeal
     
@@ -16,13 +16,14 @@ gotHeal:
 nonHeal:
     mov r3, #0
 end:
-    ldr r1, =$08019f34
+    ldr r1, =0x08019f34
     ldr r1, [r1]
     add r0, r4, r1
     ldrb r0, [r0]
     lsl r0, r0, #24
     asr r0, r0, #24
     add r0, r0, r3
-    pop {r4, lr}
-@ltorg
+    pop {r4, pc}
+.align
+.ltorg
 adr:
