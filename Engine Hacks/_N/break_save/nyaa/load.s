b4f2825584c39d6bf62b816353512b879f872c95
aera <eliwooood@gmail.com>
2019-12-16 21:14:31 +0900

    sav

---
+++ Engine Hacks/_N/break_save/nyaa/load.s
@@ -0,0 +1,60 @@
+CHAPTER_BASE_ADR = (0x0202bcec)
+.thumb
+@080a7cb0
+    push {r4, r5, r6, r7, lr}
+    .short 0xb0ac @sub sp, #176
+    .short 0xb0ac @sub sp, #176
+    nop			@消すと下の命令が異常動作する・・・
+    ldr r1, =0x080a9bb5
+    mov r0, lr
+    cmp r0, r1
+    beq normal
+    mov r0, #0
+    b merge
+    
+normal:
+	ldr r0, =CHAPTER_BASE_ADR
+	ldrb r0, [r0, #12]
+@	mov r0, r9
+    add r0, #1
+merge:
+    ldr r1, =0x160
+    mul r0, r1
+    ldr r1, =0x0E007400
+    add r1, r0, r1
+    mov r4, r1
+@ここからスキル
+    mov r1, #0
+    cmp r0, #0
+    .short 0xD100
+    mov r1, #1
+
+    mov r0, r1
+        ldr r1, ADR
+        mov lr, r1
+    .short 0xF800
+@ここまでスキル
+    mov r0, r4
+    
+    ldr r1, =0x080a7d24
+    ldr r1, [r1]
+    ldr r3, [r1]
+    mov r1, sp
+        ldr r2, =0x080d65c8 @
+        mov lr, r2
+    mov r2, #176
+    lsl r2, r2, #1
+    .short 0xF800
+    
+        ldr r0, =0x0803144c @=輸送隊のベースアドレスロード
+        mov lr, r0
+        .short 0xF800
+    mov r4, r0
+    .short 0xAD19
+    add r5, #100
+    ldr r1, =0x080a7cc8
+    mov pc, r1
+.align
+.ltorg
+ADR:
+
--- Engine Hacks/_N/break_save/nyaa/save.s
+++ Engine Hacks/_N/break_save/nyaa/save.s
@@ -1,18 +1,18 @@
-@define CHAPTER_BASE_ADR (0x0202bcec)
-@thumb
-;000a7c28
+CHAPTER_BASE_ADR = (0x0202bcec)
+.thumb
+@000a7c28
     push {r4, r5, r6, r7, lr}
     mov r7, r8
     push {r7}
-    @dcw $b0ac ;sub sp, #176
-    @dcw $b0ac ;sub sp, #176
+    .short 0xb0ac @sub sp, #176
+    .short 0xb0ac @sub sp, #176
     nop
-    ldr r1, =$080a9abd
+    ldr r1, =0x080a9abd
     mov r0, lr
     cmp r0, r1
     beq normal
     nop
-    ldr r1, =$080a99c9
+    ldr r1, =0x080a99c9
     cmp r0, r1
     beq start
     
@@ -20,8 +20,8 @@
     b merge
 start:
 	ldr r0, =CHAPTER_BASE_ADR
-	ldrb r0, [r0, 12]
-;	mov r0, r9
+	ldrb r0, [r0, #12]
+@	mov r0, r9
     add r0, #1
     b merge
 normal:
@@ -30,30 +30,29 @@ normal:
 merge:
     ldr r1, =0x160
     mul r0, r1
-    ldr r1, =$0E007400
+    ldr r1, =0x0E007400
     add r1, r0, r1
     mov r8, r1
-;ここからスキル
+@ここからスキル
     mov r1, #0
     cmp r0, #0
-    @dcw $D100
+    .short 0xD100
     mov r1, #1
 
     mov r0, r1
-        @align 4
-        ldr r1, [ADR]
+        ldr r1, ADR
         mov lr, r1
-    @dcw $F800
-    
-;ここまでスキル
-        ldr r0, =$0803144c ;=輸送隊のベースアドレスロード
+        .short 0xF800
+    nop
+@ここまでスキル
+        ldr r0, =0x0803144c @=輸送隊のベースアドレスロード
         mov lr, r0
-    @dcw $F800
+        .short 0xF800
     mov r6, r0
-    @dcw $AD19
+    .short 0xAD19
     add r5, #100
-    ldr r0, =$080a7c3a
+    ldr r0, =0x080a7c3a
     mov pc, r0
-@align 4
-@ltorg
-    ADR:
+.align
+.ltorg
+ADR:
