c861d0aa91ab4099b22bb261c162ee4398295017
aera <eliwooood@gmail.com>
2019-12-21 22:56:12 +0900

    skill wizard

---
+++ Engine Hacks/_N/_Skills/config/cleanup.bat
@@ -0,0 +1,45 @@
+@echo off
+chcp 65001
+cd /d %~dp0
+
+set /p number="ファイル数を入力してください："
+
+
+  type nul > list_definitions.event
+  echo #define ICON_LIST_SIZE (20)>> list_definitions.event
+  echo skl_table:>> list_definitions.event
+  echo SHORT 0 0 >> list_definitions.event
+  echo POIN 0 0 0 0 >> list_definitions.event
+
+  type nul > config_index.event
+
+setlocal enabledelayedexpansion
+for /l %%n in (1,1,%number%) do (
+  set num=00%%n
+  set num=!num:~-3,3!
+  type nul > skill_!num!.txt
+
+  echo skill_!num!_unit:>> skill_!num!.txt
+  echo    BYTE $00 $00 0 >> skill_!num!.txt
+
+  echo skill_!num!_class:>> skill_!num!.txt
+  echo    BYTE $00 $00 0 >> skill_!num!.txt
+
+  echo skill_!num!_weapon:>> skill_!num!.txt
+  echo    BYTE $00 $00 0 >> skill_!num!.txt
+
+  echo skill_!num!_item:>> skill_!num!.txt
+  echo    BYTE $00 $00 0 >> skill_!num!.txt
+
+  echo #define SKL_HELP_!num! $0000>> skill_!num!.txt
+  echo #define SKL_COLOR_!num! $FF>> skill_!num!.txt
+
+  echo SHORT SKL_HELP_!num! SKL_COLOR_!num! >> list_definitions.event
+  echo POIN skill_!num!_unit skill_!num!_class skill_!num!_weapon skill_!num!_item>> list_definitions.event
+
+  echo #include skill_!num!.txt >> config_index.event
+)
+endlocal
+
+  echo WORD $FFFFFFFF $FFFFFFFF $FFFFFFFF $FFFFFFFF $FFFFFFFF>> list_definitions.event
+  echo ALIGN 4 >> config_index.event
