c2f94458e9cb5c10dbdec111b64a9c9422b23589
aera <eliwooood@gmail.com>
2020-01-26 17:51:41 +0900

    ステータスアップ

---
+++ Engine Hacks/_N/_Skills/StatusUp/StatusUp.s
@@ -0,0 +1,25 @@
+.thumb
+
+main:
+    push {lr}
+    bl HasStatusUp
+    cmp r0, #0
+    beq end
+    bl GetPow
+    b end
+false:
+    mov r0, #0
+end:
+	pop {pc}
+
+GetPow:
+    ldr r0, addr+0
+    bx lr
+
+HasStatusUp:
+    ldr r2, addr+4
+    mov pc, r2
+.align
+.ltorg
+addr:
+
