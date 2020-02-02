250b0dcd55906d274c82f331961f95d518903300
aera <eliwooood@gmail.com>
2020-02-02 17:09:18 +0900

    戦技表示

--- Engine Hacks/_N/_Skills/_common/judgeOccult.s
+++ Engine Hacks/_N/_Skills/_common/judgeOccult.s
@@ -1,20 +1,18 @@
-@thumb
+.thumb
 
     push {r4, lr}
     mov r4, r0
-        @align 4
-        ldr r1, [adr+0] ;スキル
+        ldr r1, adr+0 @スキル
         mov lr, r1
-        @dcw $F800
+        .short $F800
     cmp r0, #0
     bne ret
     
     mov r0, #72
     ldrh r0, [r4, r0]
-        @align 4
-        ldr r1, [adr+4] ;武器種類
+        ldr r1, adr+4 @武器種類
         mov lr, r1
-        @dcw $F800
+        .short $F800
     cmp r0, #7
     bgt end
     add r0, #40
@@ -22,10 +20,11 @@
     cmp r0, #250
     bls end
     mov r0, #1
-    @dcw $E000
-end
-    mov r0, #0
-ret
     pop {r4, pc}
-@align 4
+end:
+    mov r0, #0
+ret:
+    pop {r4, pc}
+.align
+.ltorg
 adr:
