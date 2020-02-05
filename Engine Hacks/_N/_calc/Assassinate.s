e5a8de1971323ed2ecc85be1406cf0b2f73ec8b1
aera <eliwooood@gmail.com>
2020-02-06 07:23:32 +0900

    戦技表示

--- Engine Hacks/_N/_calc/Assassinate.s
+++ Engine Hacks/_N/_calc/Assassinate.s
@@ -11,23 +11,13 @@
             cmp r0, #1
             beq $0002ac8a
 
-            mov r1, #72
-            ldrh r0, [r3, r1]
-            cmp r0, #0
-            beq false
+            mov r0, r3
             push {r3}
-            bl GetWeaponType    @r0に武器の種類ID
+            bl OracleFunc    @武器レベル判定
             pop {r3}
             mov r2, #0
-        mov r1, r0
-        cmp r1, #0 
-        bne false   @剣以外は終了
-        mov r0, r3
-        add r0, #40
-        add r0, r0, r1
-        ldrb r0, [r0, #0]
-        cmp r0, #250
-        bls false
+            cmp r0, #0
+            beq false
 
         ldr r0, [r3, #0]
         ldr r1, [r3, #4]
@@ -40,7 +30,6 @@
         cmp r2, #0
         bne $0002ac8a
     false:
-        
         mov r0, r3
         add r0, #108
         strh r2, [r0, #0]
@@ -85,6 +74,10 @@ GetWeaponType:
 HasAssassinate:
     ldr r2, ADDR
     mov pc, r2
+OracleFunc:
+    ldr r3, ADDR+4
+    add r3, #26
+    mov pc, r3
 
 .align
 .ltorg
