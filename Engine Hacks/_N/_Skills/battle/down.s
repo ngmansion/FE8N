8f6f034b074582c3b4e9de5f8ab9d463d8ee1a64
aera <eliwooood@gmail.com>
2020-02-16 17:22:06 +0900

    切り込み

--- Engine Hacks/_N/_Skills/battle/down.s
+++ Engine Hacks/_N/_Skills/battle/down.s
@@ -4,150 +4,164 @@ STR_ADR = (67)	@書き込み先(AI1カウンタ)
 
 @ 0802bfd8
 
-	cmp	r6, #0
-	bne	START
-	ldr	r0, =0x0802bfec
-	mov	pc, r0
+    cmp	r6, #0
+    bne	START
+    ldr	r0, =0x0802bfec
+    mov	pc, r0
 START:
-	mov	r0, r6
-	mov	r1, r4
-		ldr	r2, =0x0802c134
-		mov	lr, r2
-		.short 0xF800
+    mov	r0, r6
+    mov	r1, r4
+        ldr	r2, =0x0802c134
+        mov	lr, r2
+        .short 0xF800
 @攻め側スキル→受け
-	ldrb r0, [r7, #19]
-	cmp r0, #0
-	beq negative	@自分のHP0
-	
-	mov	r0, r7
-	bl WarSkill_back
-	
-	mov	r0, r7
-	mov	r1, r6
-	bl Fury
-	
-	mov	r0, r7
-	mov	r1, r6
-	bl DoubleLion	@HP満タンの時だけなので最後
-	
-	mov	r0, r7
-	mov	r1, r6
-	bl SavageBlow
+    ldrb r0, [r7, #19]
+    cmp r0, #0
+    beq negative	@自分のHP0
+    
+    mov	r0, r7
+    bl WarSkill_back
+    
+    mov	r0, r7
+    mov	r1, r6
+    bl Fury
+    
+    mov	r0, r7
+    mov	r1, r6
+    bl DoubleLion	@HP満タンの時だけなので最後
+    
+    mov	r0, r7
+    mov	r1, r6
+    bl SavageBlow
 
-	bl Lunge
+    bl Lunge
 
-	ldrb r0, [r6, #19]
-	cmp r0, #0
-	beq negative	@相手のHP0
+    ldrb r0, [r6, #19]
+    cmp r0, #0
+    beq negative	@相手のHP0
 
-	mov	r0, r7
-	mov	r1, r6
-	bl Jadoku
+    mov	r0, r7
+    mov	r1, r6
+    bl Jadoku
 
-	mov	r0, r7
-	mov	r1, r6
-	bl Counter
+    mov	r0, r7
+    mov	r1, r6
+    bl Counter
 
 
 
 negative:
 @受け側スキル→攻め
-	ldrb r0, [r6, #19]
-	cmp r0, #0
-	beq END	@自分のHP0
-	
-	mov	r0, r6
-	mov	r1, r7
-	bl Fury
-	
-	mov	r0, r6
-	mov	r1, r7
-	bl DoubleLion	@HP満タンの時だけなので最後
-	
+    ldrb r0, [r6, #19]
+    cmp r0, #0
+    beq END	@自分のHP0
+    
+    mov	r0, r6
+    mov	r1, r7
+    bl Fury
+    
+    mov	r0, r6
+    mov	r1, r7
+    bl DoubleLion	@HP満タンの時だけなので最後
+    
 
 END:
-	pop	{r4, r5, r6, r7}
-	pop	{r0}
-	bx	r0
+    pop	{r4, r5, r6, r7}
+    pop	{r0}
+    bx	r0
 
 LUNGE_FLAG    = (0b00010000) @切り込みフラグ
 
 Lunge:
-		push {lr}
-		ldr r1, [r6]
-		ldr r2, [r6, #4]
-		ldr r1, [r1, #40]
-		ldr r2, [r2, #40]
-		orr r1, r2
-		ldr r2, =0x8000
-		tst r1, r2
-		bne endLunge      @敵将なら終了
+        push {lr}
+        ldr r1, [r6]
+        ldr r2, [r6, #4]
+        ldr r1, [r1, #40]
+        ldr r2, [r2, #40]
+        orr r1, r2
+        ldr r2, =0x8000
+        tst r1, r2
+        bne endLunge      @敵将なら終了
 
-		mov r0, r6
-		bl FodesFunc
-		cmp r0, #1
-		beq endLunge      @敵将なら終了
+        mov r0, r6
+        bl FodesFunc
+        cmp r0, #1
+        beq endLunge      @敵将なら終了
 
-		mov r0, r5
+        mov r0, r5
         add r0, #69
         ldrb r0, [r0]
         mov r1, #LUNGE_FLAG
         tst r0, r1
         beq endLunge      @持ってないので終了
 
-		ldrb r0, [r6, #16]
-		ldrb r1, [r6, #17]
-		ldrb r2, [r7, #16]
-		ldrb r3, [r7, #17]
-		strb r2, [r6, #16]   @相手
-		strb r3, [r6, #17]   @相手
-		ldr r2, =0x0203A954
-		strb r0, [r2, #14]
-		strb r1, [r2, #15]
-	endLunge:
-		pop {pc}
+        ldrb r0, [r6, #16]   @相手
+        ldrb r1, [r6, #17]   @相手
+        mov r2, r7
+        bl CanLocate
+        cmp r0, #0
+        beq endLunge    @立てない
+
+        ldrb r0, [r7, #16]
+        ldrb r1, [r7, #17]
+        mov r2, r6
+        bl CanLocate
+        cmp r0, #0
+        beq endLunge    @立てない
+
+        ldrb r0, [r6, #16]   @相手
+        ldrb r1, [r6, #17]   @相手
+        ldrb r2, [r7, #16]
+        ldrb r3, [r7, #17]
+        strb r2, [r6, #16]   @相手
+        strb r3, [r6, #17]   @相手
+        ldr r2, =0x0203A954
+        strb r0, [r2, #14]
+        strb r1, [r2, #15]
+    endLunge:
+        pop {pc}
 
 
 
 DoubleLion:
-	push	{r4, lr}
-	mov	r4, r0
-	
-	bl hasDoubleLion
-	cmp	r0, #0
-	beq falseDouble
-	
-	ldrb r1, [r4, #18]	@最大HP
-	ldrb r0, [r4, #19]	@現在HP
-	cmp r0, r1
-	blt falseDouble
-	sub r0, #1
-	strb	r0, [r4, #19]
-	mov	r0, #1
-	b	retDouble
+    push	{r4, lr}
+    mov	r4, r0
+    
+    bl hasDoubleLion
+    cmp	r0, #0
+    beq falseDouble
+    
+    ldrb r1, [r4, #18]	@最大HP
+    ldrb r0, [r4, #19]	@現在HP
+    cmp r0, r1
+    blt falseDouble
+    sub r0, #1
+    strb	r0, [r4, #19]
+    mov	r0, #1
+    b	retDouble
 falseDouble:
-	mov	r0, #0
+    mov	r0, #0
 retDouble:
-	pop	{r4, pc}
+    pop	{r4, pc}
 
 WarSkill_back:
-	mov r1, r0
+    mov r1, r0
 
-	ldrb r0, [r1, #11]
-	mov r2, #0xC0
-	and r2, r0
-	bne war_end @自軍以外は終了
+    ldrb r0, [r1, #11]
+    mov r2, #0xC0
+    and r2, r0
+    bne war_end @自軍以外は終了
 
-	add r1, #STR_ADR
-	mov r0, #0
-	strb r0, [r1]
+    add r1, #STR_ADR
+    mov r0, #0
+    strb r0, [r1]
 war_end:
     bx lr
 
 Counter:
-	push	{r4, lr}
-	mov r4, r0
-	mov r3, r1
+    push	{r4, lr}
+    mov r4, r0
+    mov r3, r1
 @	ldrb r0, [r4, #0x13]
 @	cmp r0, #0
 @	beq falseCounter		@撃破なら終了
@@ -198,21 +212,21 @@ startCounter:
 hpOkCounter:
     strb r0, [r4, #19] @現在19
     
-	mov r0, #0xD2	@妥当な音のIDが分からん
-	mov r1, #0xB8
-		ldr r2, =0x08014B50 @音
-		mov lr, r2
-		.short 0xF800
+    mov r0, #0xD2	@妥当な音のIDが分からん
+    mov r1, #0xB8
+        ldr r2, =0x08014B50 @音
+        mov lr, r2
+        .short 0xF800
     mov	r0, #1
     .short 0xE000
 falseCounter:
     mov	r0, #0
-	pop	{r4, pc}
+    pop	{r4, pc}
 
 SavageBlow:
-	push	{r4, r5, lr}
-	mov r3, r0
-	mov r4, r1
+    push	{r4, r5, lr}
+    mov r3, r0
+    mov r4, r1
 
     ldrb r0, [r3, #0xB]
     lsl r0, r0, #24
@@ -231,11 +245,11 @@ isRed2:
     
 startSavage:
     mov r0, r3
-	mov r1, r4
-	ldrb r2, [r1, #19]
-	cmp r2, #0
-	bne jumpSavege
-	mov r1, #0	@相手のHP0なら見切り貫通
+    mov r1, r4
+    ldrb r2, [r1, #19]
+    cmp r2, #0
+    bne jumpSavege
+    mov r1, #0	@相手のHP0なら見切り貫通
 jumpSavege:
         ldr r2, HAS_SAVAGE_FUNC
         mov lr, r2
@@ -243,58 +257,58 @@ jumpSavege:
     cmp r0, #0
     beq falseSavage	@死の吐息未所持なら終了
 
-		ldrb r0, [r4, #0xB]
-		mov r1, #0xC0
-		and r0, r1
-		b loopStartSavage
+        ldrb r0, [r4, #0xB]
+        mov r1, #0xC0
+        and r0, r1
+        b loopStartSavage
 
- 	loopSavage:
-		ldrb r0, [r5, #0xB]
-	loopStartSavage:
-		add r0, #1
-		bl Get_Status
-		mov r5, r0
-		cmp r5, #0
-		beq resultSavage
+     loopSavage:
+        ldrb r0, [r5, #0xB]
+    loopStartSavage:
+        add r0, #1
+        bl Get_Status
+        mov r5, r0
+        cmp r5, #0
+        beq resultSavage
 
-		ldr r0, [r5]
-		cmp r0, #0
-		beq loopSavage	@死亡判定1
-	    ldrb r0, [r5, #19] @現在19
-		cmp r0, #0
-		beq loopSavage	@死亡判定2
-		
-		ldr r0, [r5, #0xC]
-		ldr r1, =0x1002C
-		and r0, r1
-		bne loopSavage	@居ない判定+救出中
+        ldr r0, [r5]
+        cmp r0, #0
+        beq loopSavage	@死亡判定1
+        ldrb r0, [r5, #19] @現在19
+        cmp r0, #0
+        beq loopSavage	@死亡判定2
+        
+        ldr r0, [r5, #0xC]
+        ldr r1, =0x1002C
+        and r0, r1
+        bne loopSavage	@居ない判定+救出中
 
-		mov r0, #2	@within 2
-		mov r1, r4
-		mov r2, r5
-		bl CheckXY
-		cmp r0, #0
-		beq loopSavage	@no exist
+        mov r0, #2	@within 2
+        mov r1, r4
+        mov r2, r5
+        bl CheckXY
+        cmp r0, #0
+        beq loopSavage	@no exist
 
-	    ldrb r0, [r5, #19] @現在19
-	    sub r0, #10
-	    bgt hpOk2
-	    mov r0, #1
-	hpOk2:
-	    strb r0, [r5, #19] @現在19
-		b loopSavage
+        ldrb r0, [r5, #19] @現在19
+        sub r0, #10
+        bgt hpOk2
+        mov r0, #1
+    hpOk2:
+        strb r0, [r5, #19] @現在19
+        b loopSavage
 
 resultSavage:
-	mov r0, #0xB7	@妥当な音のIDが分からん
-	mov r1, #0xB8
-		ldr r2, =0x08014B50 @音
-		mov lr, r2
-		.short 0xF800
+    mov r0, #0xB7	@妥当な音のIDが分からん
+    mov r1, #0xB8
+        ldr r2, =0x08014B50 @音
+        mov lr, r2
+        .short 0xF800
     mov	r0, #1
     .short 0xE000
 falseSavage:
     mov	r0, #0
-	pop	{r4, r5, pc}
+    pop	{r4, r5, pc}
 
 CheckXY:
 @r1とr2がr0マス以内に居るならr0=TRUE
@@ -328,13 +342,13 @@ CheckXY:
         bx lr
 
 Get_Status:
-	ldr r1, =0x08019108
-	mov pc, r1
+    ldr r1, =0x08019108
+    mov pc, r1
 
 Jadoku:
-	push	{r4, lr}
-	mov r3, r0
-	mov r4, r1
+    push	{r4, lr}
+    mov r3, r0
+    mov r4, r1
 
     ldrb r0, [r3, #0xB]
     lsl r0, r0, #24
@@ -353,7 +367,7 @@ isRed:
     
 startJadoku:
     mov r0, r3
-	mov r1, r4
+    mov r1, r4
         ldr r2, ADR+8
         mov lr, r2
         .short 0xF800
@@ -368,37 +382,78 @@ startJadoku:
 hpOk:
     strb r0, [r4, #19] @現在19
     
-	mov r0, #0xB7	@妥当な音のIDが分からん
-	mov r1, #0xB8
-		ldr r2, =0x08014B50 @音
-		mov lr, r2
-		.short 0xF800
+    mov r0, #0xB7	@妥当な音のIDが分からん
+    mov r1, #0xB8
+        ldr r2, =0x08014B50 @音
+        mov lr, r2
+        .short 0xF800
     mov	r0, #1
     .short 0xE000
 falseJadoku:
     mov	r0, #0
-	pop	{r4, pc}
-	
+    pop	{r4, pc}
+    
 Fury:
-	push	{r4, lr}
-	mov	r4, r0
-		ldr	r2, ADR+4
-		mov	lr, r2
-		.short 0xF800
-	cmp	r0, #0
-	beq falseFury
-	ldrb r0, [r4, #19] @現在HP
-	sub	r0, #3
-	bgt jumpFury
-	mov	r0, #1
+    push	{r4, lr}
+    mov	r4, r0
+        ldr	r2, ADR+4
+        mov	lr, r2
+        .short 0xF800
+    cmp	r0, #0
+    beq falseFury
+    ldrb r0, [r4, #19] @現在HP
+    sub	r0, #3
+    bgt jumpFury
+    mov	r0, #1
 jumpFury:
-	strb	r0, [r4, #19]
-	mov	r0, #1
-	b	retFury
+    strb	r0, [r4, #19]
+    mov	r0, #1
+    b	retFury
 falseFury:
-	mov	r0, #0
+    mov	r0, #0
 retFury:
-	pop	{r4, pc}
+    pop	{r4, pc}
+
+CanLocate:
+@
+@r0 = Width
+@r1 = Height
+@r2 = Unit
+@
+@[out]r0=0(踏破不可)
+@
+@refer to 08025398
+@
+        push {r4, r5, r6, lr}
+        mov r4, r0
+        mov r5, r1
+        mov r6, r2
+
+@        ldr r0, =0x0202e4d4
+@        ldr r0, [r0, #0]
+@        lsl r3, r5, #2
+@        add r0, r3, r0
+@        ldr r0, [r0, #0]
+@        add r0, r0, r4
+@        ldrb r0, [r0, #0]
+@        cmp r0, #0
+@        bne falseLocate
+
+        ldr r1, =0x0202e4d8
+        ldr r1, [r1, #0]
+        lsl r2, r5, #2
+        add r1, r2, r1
+        ldr r1, [r1, #0]
+        add r1, r1, r4
+        ldrb r1, [r1, #0]
+        mov r0, r6
+            ldr r2, =0x08019174
+            mov lr, r2
+            .short 0xF800
+        .short 0xE000
+    falseLocate:
+        mov r0, #0
+        pop {r4, r5, r6, pc}
 
 
 DOUBLE_LION_ADR = (ADR+16)
@@ -406,12 +461,12 @@ HAS_SAVAGE_FUNC = (ADR+20)
 FODES_FUNC = (ADR+24)
 
 FodesFunc:
-	ldr r1, FODES_FUNC
-	mov pc, r1
+    ldr r1, FODES_FUNC
+    mov pc, r1
 
 hasDoubleLion:
-	ldr r2, DOUBLE_LION_ADR
-	mov pc, r2
+    ldr r2, DOUBLE_LION_ADR
+    mov pc, r2
 
 .align
 .ltorg
