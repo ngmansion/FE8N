30dca93bafaadebcfe4bc84f8de02a3523c22839
aera <eliwooood@gmail.com>
2020-02-28 07:17:09 +0900

    危険アイコン(重い)

--- Engine Hacks/_N/SCV/WarningAndHpBars.s
+++ Engine Hacks/_N/SCV/WarningAndHpBars.s
@@ -2,7 +2,7 @@
 OptionByte2 = 0x0202BD2D
 
     push {r7}
-    sub sp, #0x8
+    sub sp, #0x10
     mov r7, sp
 
     ldr r0, =OptionByte2
@@ -11,16 +11,12 @@ OptionByte2 = 0x0202BD2D
     tst r0, r1
     bne return
 
-    bl IsOntheScreen        @カメラ位置をキャッシュ
-    cmp r0, #0
-    beq return
+    bl Always
 
-    bl HpBars
-    bl Pinned
     bl Selecting
 
 return:
-    add sp, #0x8
+    add sp, #0x10
     pop {r7}
 mov r0, #0x30
 ldrb r0, [r4,r0]
@@ -29,6 +25,15 @@ lsr r0, #0x1C
 ldr r1, =0x08027660
 mov pc, r1
 
+Always:
+        push {lr}
+        bl CameraCacheFunc        @カメラ位置をキャッシュ
+        cmp r0, #0
+        beq falseAlways
+        bl HpBars
+        bl Pinned
+    falseAlways:
+        pop {pc}
 
 Selecting:
         push {r6, lr}
@@ -77,14 +82,205 @@ IsDanger:
         tst r0, r1
         beq falseDanger
 
-        nop
-        nop
-        nop
+        bl JudgeDanger
+        .short 0xE000
 
     falseDanger:
         mov r0, #0
         pop {pc}
 
+
+BATTLE_SIZE = (128)
+
+JudgeDanger:
+        push {lr}
+        sub sp, #BATTLE_SIZE
+
+        mov r0, #15
+        ldr r1, =0x03004df0
+        ldr r1, [r1]
+        mov r2, r4
+        bl CheckXY
+        cmp r0, #0
+        beq notDanger
+
+        ldr r0, =0x0203a4e8
+        ldr r1, =0x03004df0
+        ldr r1, [r1]
+
+        ldrb r2, [r0, #0x0B]
+        ldrb r3, [r1, #0x0B]
+        cmp r2, r3
+        beq dontNeed
+        bl SetInitDataR1toR0
+    dontNeed:
+        mov r0, sp
+        mov r1, r4
+        bl SetInitDataR1toR0
+
+        mov r0, sp
+        ldr r1, =0x0203a4e8
+        bl CALC_TRIANGLE_FUNC
+
+        ldr r0, =0x0203a4e8
+        mov r1, sp
+        bl CALC_TRIANGLE_FUNC
+
+        mov r0, sp
+        ldr r1, =0x0203a4e8
+        bl CALC_ALL_FUNC
+
+        ldr r0, =0x0203a4e8
+        mov r1, sp
+        bl CALC_ALL_FUNC
+
+        mov r0, sp
+        ldr r1, =0x0203a4e8
+        bl CALC_VERSUS_FUNC
+
+        ldr r0, =0x0203a4e8
+        mov r1, sp
+        bl CALC_VERSUS_FUNC
+
+        mov r0, #90
+        add r0, sp
+        ldrh r0, [r0]
+
+        ldr r3, =0x0203a4e8
+        mov r1, #92
+        ldrh r1, [r3, r1]
+        sub r0, r1
+        ble notDanger
+
+        lsl r0, #1
+        ldrb r1, [r3, #18]  @最大HP
+
+        cmp r1, r0
+        bgt notDanger       @HPの方が多い
+        mov r0, #1
+        .short 0xE000
+    notDanger:
+        mov r0, #0
+        add sp, #BATTLE_SIZE
+        pop {pc}
+
+
+UNIT_SIZE = (60)
+
+SetInitDataR1toR0:
+        push {r0, lr}
+        mov r2, #UNIT_SIZE
+        bl MEMCPY_R1toR0_FUNC
+        ldr r0, [sp]
+        bl Initialize
+        pop {r0, pc}
+
+Initialize:
+        push {r0, lr}
+
+        bl HitPointFunc
+        strb r0, [r1, #18]
+
+        ldr r0, [sp]
+        bl StrongFunc
+        ldr r1, [sp]
+        strb r0, [r1, #20]
+
+        ldr r0, [sp]
+        bl DefenseFunc
+        ldr r1, [sp]
+        strb r0, [r1, #23]
+
+        ldr r0, [sp]
+        bl SpeedFunc
+        ldr r1, [sp]
+        strb r0, [r1, #22]
+
+        ldr r0, [sp]
+        bl ResistFunc
+        ldr r1, [sp]
+        strb r0, [r1, #24]
+
+        ldr r0, [sp]
+        bl MagicFuncIfNeed
+
+        ldr r0, [sp]
+        bl EquipmentFunc
+        pop {r0, pc}
+
+MagicFuncIfNeed:
+        push {lr}
+        ldr r3, =0x08018ecc     @magic_func
+        ldr r1, [r3]
+        ldr r2, =0x468F4900
+        cmp r1, r2
+        bne notMagic
+
+        mov lr, r3
+        .short 0xF800
+        strb r0, [r1, #26]
+    notMagic:
+        pop {pc}
+
+EquipmentFunc:
+        push {r4, r5, r6, lr}
+        mov r6, r0
+
+        mov r2, #0
+        mov r1, #83
+        strb r2, [r6, r1]       @すくみ初期化
+        mov r1, #84
+        strb r2, [r6, r1]       @すくみ初期化
+
+        bl $080168d0
+        ldr r1, =0x0802a894
+        mov pc, r1
+
+$080168d0:
+    ldr r1, =0x080168d0
+    mov pc, r1
+$08017314:
+    ldr r1, =0x08017314
+    mov pc, r1
+
+
+
+HitPointFunc:
+    ldr r2, =0x08018ea4
+    mov pc, r2
+StrongFunc:
+    ldr r2, =0x08018ec4
+    mov pc, r2
+DefenseFunc:
+    ldr r2, =0x08018f64
+    mov pc, r2
+SpeedFunc:
+    ldr r2, =0x08018f24
+    mov pc, r2
+ResistFunc:
+    ldr r2, =0x08018f84
+    mov pc, r2
+
+CALC_TRIANGLE_FUNC:
+    ldr r2, =0x0802c6f8
+    mov pc, r2
+CALC_ALL_FUNC:
+    ldr r2, =0x0802a8c8
+    mov pc, r2
+CALC_VERSUS_FUNC:
+    ldr r2, =0x0802a914
+    mov pc, r2
+
+CALC_ATK_FUNC:
+    ldr r2, =0x0802aa28
+    mov pc, r2
+CALC_DEF_FUNC:
+    ldr r2, =0x0802a9b0
+    mov pc, r2
+
+MEMCPY_R1toR0_FUNC:
+    ldr r3, =0x080d6908
+    mov pc, r3
 
 CanTalk:
         push {lr}
@@ -185,8 +381,11 @@ y_coord = 0x11
 CameraStuff = 0x0202BCAC
 Xvalue = (0x0201)
 
-IsOntheScreen:
+CameraCacheFunc:
         push {lr}
+        mov r0, #0
+        str r0, [r7,#0x0]
+
         mov      r1,#x_coord
         ldsb  r1,[r4,r1]
         lsl      r1,#4
@@ -211,10 +410,11 @@ IsOntheScreen:
         cmp      r0,#0xB0
         bhi      falseScreen         @y is either >0xB0 or <0, so out of range
 
-        str      r3,[r7,#0x0]         @sp+4 = x - x'
-        str      r2,[r7,#0x4]         @sp+8 = y - y'
+        str      r3,[r7,#0x4]         @sp+4 = x - x'
+        str      r2,[r7,#0x8]         @sp+8 = y - y'
 @Find out whether we even need to display an hp bar
         mov r0, #1
+        str r0, [r7,#0x0]
         .short 0xE000
     falseScreen:
         mov r0, #0
@@ -236,13 +436,13 @@ HpBars:
         ldr r3, HPFramePointers
         ldr r2, [r3, r2]
 
-        ldr r0, [r7, #0]
+        ldr r0, [r7, #4]
         ldr r3, = Xvalue
         add r0, r3
         sub r3, #2      @Create 0x1FF
         and r0, r3
 
-        ldr r1, [r7, #4]
+        ldr r1, [r7, #8]
         add r1, #0xFB
         mov r3, #0xFF
         and r1, r3
@@ -266,13 +466,13 @@ Pinned:
 
         ldr r2, PinFramePointers
 
-        ldr r0, [r7, #0]
+        ldr r0, [r7, #4]
         ldr r3, = Xvalue
         add r0, r3
         sub r3, #2      @Create 0x1FF
         and r0, r3
 
-        ldr r1, [r7, #4]
+        ldr r1, [r7, #8]
         add r1, #0xFB
         mov r3, #0xFF
         and r1, r3
@@ -289,18 +489,21 @@ WRAMDisplay = 0x08002B08
 Draw_Warning_Sign:
 @r0=thing to determine what we're drawing, r1=sp (to retrieve x and y stuff)
         push {lr}
+        ldr r2, [r7, #0]
+        cmp r2, #0
+        beq FinishedDrawing
 
         ldr r2, WS_FrameData
         add r2, r0
 
         ldr    r1, =0x0201
-        ldr    r0,[r7,#0]        @x-x'
+        ldr    r0,[r7,#4]        @x-x'
         add    r0,r1
         add    r0,#0xB              @tweak for x coordinate, whatever that means
         sub    r1,#2
         and    r0,r1
 
-        ldr    r1,[r7,#4]        @y-y'
+        ldr    r1,[r7,#8]        @y-y'
         add    r1,#0xEE          @y coordinate tweak?
         mov    r3,#0xFF
         and    r1,r3
@@ -309,8 +512,37 @@ Draw_Warning_Sign:
         mov    lr, r3
         mov    r3,#0
         .short 0xF800
-
+FinishedDrawing:
         pop {pc}
+
+CheckXY:
+@r1とr2がr0マス以内に居るならr0=TRUE
+@同座標ならTRUE
+@
+        push {r0}
+        ldrb r0, [r1, #16]
+        ldrb r3, [r2, #16]
+        sub r3, r0, r3
+        bge jump1CheckXY
+        neg r3, r3  @絶対値取得
+    jump1CheckXY:
+
+        ldrb r1, [r1, #17]
+        ldrb r2, [r2, #17]
+        sub r2, r1, r2
+        bge jump2CheckXY
+        neg r2, r2  @絶対値取得
+    jump2CheckXY:
+
+        add r2, r2, r3
+        pop {r0}
+        cmp r2, r0
+        bgt falseCheckXY    @r0マス以内に居ない
+        mov r0, #1
+        bx lr
+    falseCheckXY:
+        mov r0, #0
+        bx lr
 
 HPFramePointers = addr+0
 PinFramePointers = addr+4
