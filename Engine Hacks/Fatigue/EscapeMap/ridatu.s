97b9046f3bdb14acadbd4fb74d9c77a441f49ad5
aera <eliwooood@gmail.com>
2020-03-07 01:45:33 +0900

    離脱

---
+++ Engine Hacks/Fatigue/EscapeMap/ridatu.s
@@ -0,0 +1,42 @@
+.thumb
+
+@400D0000
+    push {lr}
+    ldr  r0, =0x03004df0
+    ldr  r0, [r0]
+    ldrb r1, [r0, #0xB]
+    lsr  r1, r1, #6
+    bne  end                            @自軍ではないなら終了
+
+    ldrb r1, [r0, #0xC]
+    lsl  r1, r1, #27
+    bpl  skip                           @担いでいない
+    ldrb r0, [r0, #0x1B]
+        ldr  r1, =0x08019108
+        mov  lr, r1
+        .short 0xF800
+    ldr  r1, [r0]
+    ldrb r1, [r1, #4]
+    ldr  r2, =0x030004B0
+    ldr  r2, [r2, #4]
+    cmp  r1, r2
+    beq  end
+    
+    bl Stay
+skip:
+    ldr  r0, =0x03004df0
+    ldr  r0, [r0]
+    bl Stay
+end:
+    pop  {pc}
+
+
+Stay:
+    ldr r1, [r0, #0xC]
+    ldr r2, =0x01000008
+    orr r1, r2
+    str r1, [r0, #0xC]
+    mov r1, #0xFF
+    strb r1, [r0, #16]
+    bx lr
+
---
+++ Engine Hacks/Fatigue/EscapeMap/ridatu_back.s
@@ -0,0 +1,34 @@
+UNIT_MAX_NUM = (51)
+
+.thumb
+
+    push {r5, lr}
+    mov r5, #0
+outLoopUnit:
+    add r5, #1
+    cmp r5, #UNIT_MAX_NUM
+    blt return
+    
+    mov r0, r5
+        ldr r1, =0x08019108
+        mov lr, r1
+        .short 0xF800
+    ldr r1, [r0]
+    cmp r1, #0
+    beq return   @ユニットがもういない
+    ldr r1, [r0, #0xC]
+    ldr r2, =0x01000008
+    mov r3, r1
+    and r3, r2
+    cmp r3, r2
+    bne outLoopUnit
+    bic r1, r2
+    str r1, [r0, #0xC]
+    b outLoopUnit
+return:
+    pop {r5, pc}
+
+.align
+.ltorg
+ADDR:
+
