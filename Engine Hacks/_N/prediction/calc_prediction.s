1c366096e6bfc4565b4b1b9a431d7d6b0e405c8a
aera <eliwooood@gmail.com>
2020-03-08 20:25:27 +0900

    戦闘結果予測強化

---
+++ Engine Hacks/_N/prediction/calc_prediction.s
@@ -0,0 +1,78 @@
+.thumb
+
+    cmp r0, #0
+    bne switch
+    ldr r0, =0x0202bd2e
+    ldrb r1, [r0]
+    mov r2, #0x18
+    bic r1, r2
+    mov r2, #0x08
+    orr r1, r2
+    strb r1, [r0]
+
+    ldr r0, =0x0203a568
+    ldr r1, =0x0203a4e8
+    .short 0xE001
+
+switch:
+    ldr r0, =0x0203a4e8
+    ldr r1, =0x0203a568
+
+    bl main
+    mov r2, r0
+    mov r0, r5
+    add r0, #128
+    mov r1, #2
+
+    ldr r3, addr+4
+    mov pc, r3
+
+
+
+
+main:
+push {r4, r5, lr}
+mov r5, r0
+mov r3, r1
+
+add	r0, #90
+mov	r2, #0
+ldsh	r1, [r0, r2]
+
+mov	r0, #92
+ldsh	r0, [r0, r3]
+sub	r0, r1, r0
+
+mov r1, r5
+mov r2, r3
+bl DEF_DIVIDE
+mov	r4, r0
+
+mov r0, #72
+ldrh	r0, [r5, r0]
+bl	$00017294
+cmp	r0, #181	@ストーン
+bne	$00036a62
+mov	r4, #255
+
+$00036a62:
+cmp	r1, #0
+bge	$00036a6c
+mov	r4, #0
+
+$00036a6c:
+mov r0, r4
+pop {r4, r5, pc}
+
+
+$00017294:
+ldr r1, =0x08017294
+mov pc, r1
+
+DEF_DIVIDE:
+ldr r3, addr
+mov pc, r3
+
+.align
+.ltorg
+addr:
