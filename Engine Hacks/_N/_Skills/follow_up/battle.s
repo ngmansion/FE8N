af62a67ed36349a99c9e89471bc04dad6a02cc43
aera <eliwooood@gmail.com>
2020-10-13 23:03:31 +0900

    Fix #31

--- Engine Hacks/_N/_Skills/follow_up/battle.s
+++ Engine Hacks/_N/_Skills/follow_up/battle.s
@@ -5,11 +5,20 @@
     orr r0, r1
     str r0, [r3, #0]
 
+    ldr r0, =0x0203a4d0
+    ldrh r0, [r0]
+    mov r1, #0x20
+    and r0, r1
+    bne normalBattle        @闘技場なら通常
+
     b JudgeSemetate
 retSemetate:
     cmp r0, #1
-    beq reverse
+    beq normalDesperation
+    cmp r0, #2
+    beq reverseDesperation
 
+normalBattle:
     ldr r3, [r6, #0]
     ldr r2, [r3, #0]
     lsl r1, r2, #13
@@ -30,17 +39,20 @@ retSemetate:
     bne battle_end      @試合終了
 
 
-    ldr r0, =0x0203a4e8
-    ldr r1, =0x0203a568
+    ldr r0, [sp, #0]
+    ldr r1, [sp, #4]
     bl FollowUpBattle   @三手目
     cmp r0, #0
     bne battle_end      @試合終了
-    b force_battle
 
-reverse:        @攻め立て発動。
+    ldr r0, [sp, #4]
+    ldr r1, [sp, #0]
+    bl FollowUpBattle       @四手目
+    b battle_end
 
-    ldr r0, =0x0203a4e8
-    ldr r1, =0x0203a568
+normalDesperation:        @攻め立て発動。
+    ldr r0, [sp, #0]
+    ldr r1, [sp, #4]
     bl FollowUpBattle       @三手目
     cmp r0, #0
     bne battle_end      @試合終了
@@ -64,11 +76,41 @@ reverse:        @攻め立て発動。
     cmp r0, #0
     bne battle_end      @試合終了
 
-force_battle:
-
-    ldr r1, =0x0203a4e8
-    ldr r0, =0x0203a568
+    ldr r0, [sp, #4]
+    ldr r1, [sp, #0]
     bl FollowUpBattle       @四手目
+    b battle_end
+
+reverseDesperation:
+    ldr r3, [r6, #0]
+    ldr r2, [r3, #0]
+    lsl r1, r2, #13
+    lsr r1, r1, #13
+    mov r0, #8          @反撃
+    orr r1, r0
+    ldr r5, =0xfff80000
+
+    mov r0, r5
+    and r0, r2
+    orr r0, r1
+    str r0, [r3, #0]
+
+    ldr r0, [sp, #4]
+    ldr r1, [sp, #0]
+    bl $0002af88        @二手目
+    cmp r0, #0
+    bne battle_end      @試合終了
+
+    ldr r0, [sp, #4]
+    ldr r1, [sp, #0]
+    bl FollowUpBattle       @四手目
+    cmp r0, #0
+    bne battle_end      @試合終了
+
+    ldr r0, [sp, #0]
+    ldr r1, [sp, #4]
+    bl FollowUpBattle       @三手目
+    b battle_end
 
 battle_end:         @敵機撃破
     ldr r1, =0x0802aec0
@@ -123,17 +165,23 @@ JudgeSemetate:
         and r0, r1
         bne falseSemetate      @闘技場なら終了
 
+        ldr r0, =0x0203a4e8     @攻めユニット
+        ldr r1, =0x0203a568     @受けユニット
+        bl HAS_SEMETATE
+        cmp r0, #0
+        beq falseSemetate      @攻め立て無し終了
+
         ldr r0, =0x0203a4e8
         ldr r1, [sp]
         cmp r0, r1
-        bne falseSemetate      @先制攻撃者と加攻撃者が一致しないなら終了(待ち伏せ対策)
-
-        ldr r0, [sp]
-        ldr r1, [sp, #4]
-        bl HAS_SEMETATE
-        .short 0xE000
+        bne reverseSemetate     @先制攻撃者と攻めユニットが一致しない(=待ち伏せされた)ならジャンプ
+        mov r0, #1
+        b retSemetate
     falseSemetate:
         mov r0, #0
+        b retSemetate
+    reverseSemetate:
+        mov r0, #2
         b retSemetate
 
 $0002af88:
