668d9a0f7604c3b630e0d80f40e91ca1c1a6a4ab
aera <eliwooood@gmail.com>
2021-04-17 17:03:32 +0900

    #78 Add

--- Engine Hacks/_N/_Skills/Savior/IsRescue.s
+++ Engine Hacks/_N/_Skills/Savior/IsRescue.s
@@ -6,58 +6,86 @@
 
 @r0はユニットアドレス
     pop {r0}
-    push {r1, r2, r3, r4, lr}
-    mov r4, r0
-    bl Savior
-    cmp r0, #1
-    beq false
-
-    bl rescue_flag
-    b RETURN
-false:
-    mov r0, #0
-RETURN:
+    push {r1, r2, r3, lr}
+    bl main
     cmp r0, #0
-    pop {r1, r2 ,r3, r4, pc}
+    pop {r1, r2 ,r3, pc}
 
-Savior:
-@スキル所持and味方救出中ならTRUE
-@
-        push {lr}
-
-        mov r0, r4
-        mov r1, #0
-        bl HasSavior
-        cmp r0, #0
-        beq falseSavior
+main:
+@救出中ならTRUE
+        push {r4, lr}
+        mov r4, r0
 
         ldr r0, [r4, #12]
         mov r1, #0x10
         and r0, r1
-        beq falseSavior @救出してない
+        beq falseMain
         ldrb r0, [r4, #27]
         cmp r0, #0
-        beq falseSavior   @救出してない
+        beq trueMain  @問答無用でtrue
 
+        mov r0, r4
+        mov r1, #0
+        bl HasSavior
+        cmp r0, #1
+        beq false
+
+        bl CompareConstitution
+        b end
+    trueMain:
         mov r0, #1
-        b endSavior
-    falseSavior:
+        .short 0xE000
+    falseMain:
         mov r0, #0
-    endSavior:
+    end:
+        pop {r4, pc}
+
+CompareConstitution:
+        push {lr}
+
+        ldrb r0, [r4, #27]
+        cmp r0, #0
+        beq trueCompare
+        ldr r1, =0x08019108
+        mov lr, r1
+        .short 0xF800
+
+        ldr r1, [r0, #4]
+        ldr r0, [r0, #0]
+
+        ldrb r1, [r1, #17]   @クラス体格
+        lsl r1, r1, #24
+        asr r1, r1, #24
+
+        ldrb r0, [r0, #19]   @ユニット体格
+        lsl r0, r0, #24
+        asr r0, r0, #24
+        add r0, r1
+
+        ldr r1, [r4, #4]
+        ldr r2, [r4, #0]
+
+        ldrb r1, [r1, #17]   @クラス体格
+        lsl r1, r1, #24
+        asr r1, r1, #24
+
+        ldrb r2, [r2, #19]   @ユニット体格
+        lsl r2, r2, #24
+        asr r2, r2, #24
+        add r2, r1
+    @され側の体格加算
+        add r0, #4
+#        lsl r0, #1
+        cmp r2, r0
+        blt trueCompare
+        mov r0, #0
+        .short 0xE000
+
+    trueCompare:
+        mov r0, #1
         pop {pc}
 
 
-rescue_flag:
-        ldr r0, [r4, #12]
-        mov r1, #16
-        and r0, r1
-        beq false_flag
-        mov r0, #1
-        b end_flag
-    false_flag:
-        mov r0, #0
-    end_flag:
-        bx lr
 
 HasSavior:
 ldr r2, addr
