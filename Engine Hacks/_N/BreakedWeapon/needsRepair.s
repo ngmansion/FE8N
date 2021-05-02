228f0f96b60dc5751595429b10dba6cce7332c8f
aera <eliwooood@gmail.com>
2021-05-02 23:18:00 +0900

    #83

--- Engine Hacks/_N/BreakedWeapon/needsRepair.s
+++ Engine Hacks/_N/BreakedWeapon/needsRepair.s
@@ -10,20 +10,20 @@
     beq TRUE
 default:
     ldr r0, [r1, #8]
-	lsl r1, r0, #28
-	bmi TRUE	@回数無限
-	lsl r1, r0, #21
-	bmi TRUE	@竜石
-	mov r1, #0x6	@魔法または杖
-	and r0, r1
-	bne TRUE
+    lsl r1, r0, #28
+    bmi TRUE    @回数無限
+    lsl r1, r0, #21
+    bmi TRUE    @竜石
+    mov r1, #0x6    @魔法または杖
+    and r0, r1
+    bne TRUE
 FALSE:
-	mov r0, #0
-	b RETURN
+    mov r0, #0
+    b RETURN
 TRUE:
-	mov r0, #1
+    mov r0, #1
 RETURN:
-	bx lr
-	
-	
-	
+    bx lr
+    
+    
+    
