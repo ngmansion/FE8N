e70009e0affd3361c0c9e85bf4431203d37393d6
aera <eliwooood@gmail.com>
2021-05-23 23:13:07 +0900

    1距離武器以外で戦技を選択した時の射程の表示が誤っている #85 再修正

--- Engine Hacks/_N/_Skills/CombatArts/Selector.s
+++ Engine Hacks/_N/_Skills/CombatArts/Selector.s
@@ -1,10 +1,10 @@
 .thumb
-
-push	{r4, r5, r6, lr}
-mov	r5, r0
+@ 08050008
+push {r4, r5, r6, lr}
+mov r5, r0
 bl main
-mov	r2, r5
-add	r2, #97
+mov r2, r5
+add r2, #97
 ldr r0, =0x08050010
 mov pc, r0
 
@@ -32,15 +32,15 @@ main:
         bl Sound
         pop {pc}
 
-KEY_RIGHT = (16)
-KEY_LEFT = (32)
+key_right = (16)
+key_left = (32)
 
 Left:
         push {lr}
         ldr r1, =0x085775cc
         ldr r3, [r1, #0]
         ldrh r0, [r3, #6]
-        mov r1, #KEY_LEFT
+        mov r1, #key_left
         and r0, r1
         cmp r0, #0
         beq falseMove
@@ -53,7 +53,7 @@ Right:
         ldr r1, =0x085775cc
         ldr r3, [r1, #0]
         ldrh r0, [r3, #6]
-        mov r1, #KEY_RIGHT
+        mov r1, #key_right
         and r0, r1
         cmp r0, #0
         beq falseMove
@@ -126,62 +126,75 @@ DrawRangeMap:
 
 DrawWeaponRangeMap:
 @選択中武器の射程表示
-    push {r4, r5, lr}
-    add r5, #0x61
-    ldr r0, =0x08022d5a
-    mov pc, r0
+        push {r4, r5, lr}
+    @0804FCDEの処理
+    @
+        mov r0, r4
+        add r0, #97
+        ldrb r0, [r0, #0]
+        lsl r0, r0, #2
+        mov r1, r4
+        add r1, #52
+        add r1, r1, r0
+        ldr r1, [r1, #0]
+    @08022d4cの処理
+    @
+        mov r5, r1
+        add r5, #60
+        ldr r0, =0x08022d5a
+        mov pc, r0
 
 DrawSingleRangeMap:
-@選択中武器に関わらず直接攻撃射程
-    push    {r4, r5, lr}
-@    mov r5, r1
-@    add r5, #60
-@    mov r0, #0
-@    ldsb    r0, [r5, r0]
-@    ldr r2, =0x0801e3a4    @ステータス上書き
-@    mov lr, r2
-@    .short 0xF800
-    ldr r0, =0x0202e4dc
-    ldr r0, [r0, #0]
-    mov r1, #1
-    neg r1, r1
-    ldr r2, =0x080194bc
-    mov lr, r2
-    .short 0xF800
-    ldr r0, =0x0202e4e0
-    ldr r0, [r0, #0]
-    mov r1, #0
-    ldr r2, =0x080194bc
-    mov lr, r2
-    .short 0xF800
-    ldr r4, =0x03004df0
-    ldr r0, [r4, #0]
-    mov r1, #1          @常に1
-    ldr r0, [r4, #0]
-    ldr r2, =0x0801b13c
-    mov lr, r2
-    .short 0xF800
-    mov r0, #2
-    ldr r2, =0x0801d6fc
-    mov lr, r2
-    .short 0xF800
-    mov r0, #0
-    pop {r4, r5}
-    pop {r1}
-    bx  r1
+@選択中武器に関わらず直接攻撃射程      @08022d4cの処理
+        push    {r4, r5, lr}
+@        mov r5, r1
+@        add r5, #60
+@        mov r0, #0
+@        ldsb    r0, [r5, r0]
+@        ldr r2, =0x0801e3a4    @ステータス上書き
+@        mov lr, r2
+@        .short 0xF800
+        ldr r0, =0x0202e4dc
+        ldr r0, [r0, #0]
+        mov r1, #1
+        neg r1, r1
+        ldr r2, =0x080194bc
+        mov lr, r2
+        .short 0xF800
+        ldr r0, =0x0202e4e0
+        ldr r0, [r0, #0]
+        mov r1, #0
+        ldr r2, =0x080194bc
+        mov lr, r2
+        .short 0xF800
+        ldr r4, =0x03004df0
+        ldr r0, [r4, #0]
+        mov r1, #1          @常に1
+        ldr r0, [r4, #0]
+        ldr r2, =0x0801b13c
+        mov lr, r2
+        .short 0xF800
+        mov r0, #2
+        ldr r2, =0x0801d6fc
+        mov lr, r2
+        .short 0xF800
+        mov r0, #0
+        pop {r4, r5}
+        pop {r1}
+        bx  r1
 
 
 CheckHelp:  @ヘルプ中は動けなくする
         ldr r2, =0x0203E7AA
         ldrb r0, [r2]
         ldrb r1, [r2, #1]
-            cmp r0, #0
-            bne falseHelp
-            cmp r1, #0
-            beq trueHelp
-            cmp r1, #6
-            beq trueHelp
-            b falseHelp
+        cmp r0, #0
+        bne falseHelp
+        cmp r1, #0
+        beq trueHelp
+        cmp r1, #6
+        beq trueHelp
+        b falseHelp
     trueHelp:
         mov r0, #1
         .short 0xE000
